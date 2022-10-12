038eb0f041c5f390abdace36cc5b380a649fde6a
haruboting <raiu_hk@icloud.com>
2022-10-13 01:45:54 +0900

    solve

---
+++ ABC269/A.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#define all(v) v.begin(), v.end()
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define repp(i, m, n) for (int i = m; i < (int)(n); i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    cout << (a + b) * (c - d) << endl;
+    cout << "Takahashi" << endl;
+}
