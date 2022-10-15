15041302be81b2c2f437b42dd509d588f5a2fd27
haruboting <raiu_hk@icloud.com>
2022-10-16 01:50:38 +0900

    solve

---
+++ ABC273/A.cpp
@@ -0,0 +1,16 @@
+#include "bits/stdc++.h"
+#define all(v) v.begin(), v.end()
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define repp(i, m, n) for (int i = m; i < (int)(n); i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+    int N;
+    cin >> N;
+    int ans = 1;
+    rep(i, N) {
+        ans *= (i + 1);
+    }
+    cout << ans << endl;
+}
