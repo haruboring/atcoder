b22f8993aaaa596a524f3cae955c7486ed720cf9
haruboting <raiu_hk@icloud.com>
2022-10-17 02:31:11 +0900

    add ABC folder

---
+++ ABC/D.cpp
@@ -0,0 +1,9 @@
+#include "bits/stdc++.h"
+#define all(v) v.begin(), v.end()
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define repp(i, m, n) for (int i = m; i < (int)(n); i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+}
