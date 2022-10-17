00a09eba6613f64334cc48ecf6fbd6a572c4956f
haruboting <raiu_hk@icloud.com>
2022-10-18 02:02:24 +0900

    solve

--- ABC272/B.cpp
+++ ABC272/B.cpp
@@ -6,4 +6,31 @@ using namespace std;
 using ll = long long;
 
 int main() {
+    int N, M; cin >> N >> M;
+    vector<vector<int>> kx(M, vector<int>(0));
+    rep(i, M){
+        int ki;
+        cin >> ki;
+        kx[i].push_back(ki);
+        rep(j, ki){
+            int xi;
+            cin >> xi;
+            kx[i].push_back(xi);
+        }
+    }
+    set<pair<int, int>> S;
+    rep(i, M){
+        rep(j, kx[i].size() - 2){
+            rep(k, kx[i].size() - 2 - j){
+                pair<int, int> p = make_pair(kx[i][j + 1], kx[i][j + k + 2]);
+                S.insert(p);
+            }
+        }
+    }
+    if(int(S.size()) == N*(N-1)/2){
+        cout << "Yes" << endl;
+    }
+    else{
+        cout << "No" << endl;
+    }
 }
