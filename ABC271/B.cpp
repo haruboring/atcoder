683a395a38f8790240db0c15594eba40d81cb778
haruboting <raiu_hk@icloud.com>
2022-10-18 14:41:16 +0900

    solve

--- ABC271/B.cpp
+++ ABC271/B.cpp
@@ -6,4 +6,21 @@ using namespace std;
 using ll = long long;
 
 int main() {
+    int N, Q;
+    cin >> N >> Q;
+    vector<vector<int>> aa(0, vector<int>(0));
+    rep(i, N) {
+        int L;
+        cin >> L;
+        vector<int> a(L);
+        rep(j, L) {
+            cin >> a[j];
+        }
+        aa.push_back(a);
+    }
+    rep(i, Q) {
+        int s, t;
+        cin >> s >> t;
+        cout << aa[s - 1][t - 1] << endl;
+    }
 }
