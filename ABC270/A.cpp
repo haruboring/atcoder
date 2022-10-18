7677345fdbd72ff533cb5f6f63fb85588b395418
haruboting <raiu_hk@icloud.com>
2022-10-19 03:21:20 +0900

    solve

--- ABC270/A.cpp
+++ ABC270/A.cpp
@@ -6,4 +6,42 @@ using namespace std;
 using ll = long long;
 
 int main() {
+    int A, B;
+    cin >> A >> B;
+    set<int> answerd_A, answerd_B;
+    int ans=0;
+    while (A > 0) {
+        if (A - 4 >= 0) {
+            answerd_A.insert(4);
+            A -= 4;
+        } else if (A - 2 >= 0) {
+            answerd_A.insert(2);
+            A -= 2;
+        } else {
+            answerd_A.insert(1);
+            A--;
+        }
+    }
+    while (B > 0) {
+        if (B - 4 >= 0) {
+            answerd_B.insert(4);
+            B -= 4;
+        } else if (B - 2 >= 0) {
+            answerd_B.insert(2);
+            B -= 2;
+        } else {
+            answerd_B.insert(1);
+            B--;
+        }
+    }
+    if (answerd_A.count(1) || answerd_B.count(1)) {
+        ans++;
+    }
+    if (answerd_A.count(2) || answerd_B.count(2)) {
+        ans+=2;
+    }
+    if (answerd_A.count(4) || answerd_B.count(4)) {
+        ans+=4;
+    }
+    cout << ans << endl;
 }
