a51ec037ac81a6337fd5377596dac5291e33d47e
haruboting <raiu_hk@icloud.com>
2022-10-23 01:25:03 +0900

    solve

--- ABC269/B.cpp
+++ ABC269/B.cpp
@@ -6,4 +6,21 @@ using namespace std;
 using ll = long long;
 
 int main() {
+    vector<string> S(10);
+    rep(i, 10) {
+        cin >> S[i];
+    }
+    int A = 11, B = 0, C = 11, D = 0;
+    rep(i, 10) {
+        rep(j, 10) {
+            if (S[i][j] == '#') {
+                A = min(A, i + 1);
+                B = max(B, i + 1);
+                C = min(C, j + 1);
+                D = max(D, j + 1);
+            }
+        }
+    }
+    cout << A << " " << B << endl;
+    cout << C << " " << D << endl;
 }
