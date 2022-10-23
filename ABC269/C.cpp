a83c1c9041ebff6c132453bbd8cdffadc77f6f4d
haruboting <raiu_hk@icloud.com>
2022-10-24 03:54:11 +0900

    solve

--- ABC269/C.cpp
+++ ABC269/C.cpp
@@ -6,4 +6,26 @@ using namespace std;
 using ll = long long;
 
 int main() {
+    ll N;
+    cin >> N;
+    ll Ncopy = N;
+    vector<ll> ans(0);
+    ans.push_back(0);
+    ll bit_cnt = 1;
+    while (1) {
+        if (bit_cnt > Ncopy) {
+            break;
+        }
+        if (N % 2 == 1) {
+            int size = ans.size();
+            rep(i, size) {
+                ans.push_back(ans[i] + bit_cnt);
+            }
+        }
+        N /= 2;
+        bit_cnt *= 2;
+    }
+    rep(i, ans.size()) {
+        cout << ans[i] << endl;
+    }
 }
