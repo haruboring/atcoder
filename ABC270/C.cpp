6fd35442bdd3e0ebc051f54dde67fdbd61421dbf
haruboting <raiu_hk@icloud.com>
2022-10-25 02:17:13 +0900

    solve (copy ans)

--- ABC270/C.cpp
+++ ABC270/C.cpp
@@ -5,5 +5,51 @@
 using namespace std;
 using ll = long long;
 
-int main() {
+vector<vector<int>> e(200010, vector<int>(0));
+vector<bool> flag(200010);
+deque<int> deq;
+bool stop;
+
+void dfs(int position, int goal) {
+    if (!stop) {
+        deq.push_back(position);
+    }
+    if (position == goal) {
+        stop = true;
+    }
+    flag[position] = true;
+    int sz = e[position].size();
+    for (int i = 0; i < sz; i++) {
+        if (!flag[e[position][i]]) {
+            dfs(e[position][i], goal);
+        }
+    }
+    if (!stop) deq.pop_back();
+    return;
+}
+
+int main(void) {
+    int N, X, Y;
+    cin >> N >> X >> Y;
+    int U, V;
+    rep(i, N - 1) {
+        cin >> U >> V;
+        e[U].push_back(V);
+        e[V].push_back(U);
+    }
+    repp(i, 1, N + 1) {
+        flag[i] = false;
+    }
+    stop = false;
+    dfs(X, Y);
+
+    while (!deq.empty()) {
+        cout << deq.front();
+        deq.pop_front();
+        if (deq.empty())
+            cout << endl;
+        else
+            cout << " ";
+    }
+    return 0;
 }
