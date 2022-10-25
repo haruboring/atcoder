b289550bd4d56f029317876de4edc2f0b9c796b1
haruboting <raiu_hk@icloud.com>
2022-10-25 13:29:14 +0900

    solve

--- ABC268/C.cpp
+++ ABC268/C.cpp
@@ -6,4 +6,29 @@ using namespace std;
 using ll = long long;
 
 int main() {
+    int N;
+    cin >> N;
+    vector<int> p(N);
+    rep(i, N) {
+        cin >> p[i];
+    }
+
+    vector<int> dis(N);
+    rep(i, N) {
+        dis[i] = (p[i] - i + N) % N;
+    }
+
+    vector<int> dis_cnt(N);
+    rep(i, N) {
+        dis_cnt[dis[i]]++;
+    }
+
+    int max_happy = 0;
+    dis_cnt.push_back(dis_cnt[0]);
+    dis_cnt.push_back(dis_cnt[1]);
+    rep(i, N) {
+        max_happy = max(max_happy, dis_cnt[i] + dis_cnt[i + 1] + dis_cnt[i + 2]);
+    }
+
+    cout << max_happy << endl;
 }
