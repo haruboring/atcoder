e6c9eba007793fa196ab35c06a93851387f75b5e
haruboting <raiu_hk@icloud.com>
2022-10-26 01:45:20 +0900

    solve

--- ABC266/C.cpp
+++ ABC266/C.cpp
@@ -6,4 +6,27 @@ using namespace std;
 using ll = long long;
 
 int main() {
+    vector<int> x(4), y(4);
+    rep(i, 4) {
+        cin >> x[i] >> y[i];
+    }
+    bool is_totsu = true;
+
+    if (((y[0] - y[2]) * (x[1] - x[2]) - (x[0] - x[2]) * (y[1] - y[2])) *
+            ((y[0] - y[2]) * (x[3] - x[2]) - (x[0] - x[2]) * (y[3] - y[2])) >
+        0) {
+        is_totsu = false;
+    }
+
+    if (((y[1] - y[3]) * (x[0] - x[3]) - (x[1] - x[3]) * (y[0] - y[3])) *
+            ((y[1] - y[3]) * (x[2] - x[3]) - (x[1] - x[3]) * (y[2] - y[3])) >
+        0) {
+        is_totsu = false;
+    }
+
+    if (is_totsu) {
+        cout << "Yes" << endl;
+    } else {
+        cout << "No" << endl;
+    }
 }
