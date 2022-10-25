aafef085eb488a41f1c297e20d40212795a472f2
haruboting <raiu_hk@icloud.com>
2022-10-25 22:10:40 +0900

    solve

--- ABC267/C.cpp
+++ ABC267/C.cpp
@@ -6,4 +6,23 @@ using namespace std;
 using ll = long long;
 
 int main() {
+    int N, M;
+    cin >> N >> M;
+    vector<ll> A(N);
+    rep(i, N) {
+        cin >> A[i];
+    }
+    ll max_sum = 0;
+    ll sub = 0;
+    rep(i, M) {
+        max_sum += (i + 1) * A[i];
+        sub += A[i];
+    }
+    ll sum = max_sum;
+    rep(i, N - M) {
+        sum = sum - sub + M * A[M + i];
+        max_sum = max(max_sum, sum);
+        sub = sub - A[i] + A[M + i];
+    }
+    cout << max_sum << endl;
 }
