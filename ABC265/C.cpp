c32c21536bc25de5886f5d593751d9bc7db43c78
haruboting <raiu_hk@icloud.com>
2022-10-26 16:01:54 +0900

    solve

--- ABC265/C.cpp
+++ ABC265/C.cpp
@@ -6,4 +6,31 @@ using namespace std;
 using ll = long long;
 
 int main() {
+    int H, W;
+    cin >> H >> W;
+    vector<string> G(H);
+    rep(i, H) {
+        cin >> G[i];
+    }
+    int loop_cnt = 0;
+    int x = 0, y = 0;
+    while (1) {
+        if (loop_cnt > H * W + 10) {
+            cout << -1 << endl;
+            return 0;
+        }
+        if (G[x][y] == 'U' && x != 0) {
+            x--;
+        } else if (G[x][y] == 'D' && x != H - 1) {
+            x++;
+        } else if (G[x][y] == 'L' && y != 0) {
+            y--;
+        } else if (G[x][y] == 'R' && y != W - 1) {
+            y++;
+        } else {
+            break;
+        }
+        loop_cnt++;
+    }
+    cout << x + 1 << " " << y + 1 << endl;
 }
