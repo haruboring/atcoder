d8705f7fa4a012441d725ddaceb1730e84aa3bd7
haruboting <raiu_hk@icloud.com>
2022-10-26 15:48:11 +0900

    solve

--- ABC265/B.cpp
+++ ABC265/B.cpp
@@ -6,4 +6,35 @@ using namespace std;
 using ll = long long;
 
 int main() {
+    ll N, M, T;
+    cin >> N >> M >> T;
+    vector<ll> A(N - 1), X(M), Y(M);
+    rep(i, N - 1) {
+        cin >> A[i];
+    }
+    rep(i, M) {
+        cin >> X[i] >> Y[i];
+    }
+    rep(i, M) {
+        A[X[i] - 1] -= Y[i];
+    }
+    ll step = 0, time = T;
+    bool reached_N = false;
+    while (1) {
+        if (step == N - 1) {
+            reached_N = true;
+            break;
+        }
+        if (time > A[step]) {
+            time -= A[step];
+            step++;
+        } else {
+            break;
+        }
+    }
+    if (reached_N) {
+        cout << "Yes" << endl;
+    } else {
+        cout << "No" << endl;
+    }
 }
