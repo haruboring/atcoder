d65693e7d3c71eaa726fc8c0dda27b4909780cca
haruboting <raiu_hk@icloud.com>
2022-10-29 01:31:57 +0900

    solve

--- ABC262/C.cpp
+++ ABC262/C.cpp
@@ -6,4 +6,26 @@ using namespace std;
 using ll = long long;
 
 int main() {
+    int N;
+    cin >> N;
+    vector<vector<int>> a_i(N, vector<int>(2));
+    int a;
+    ll same_num_cnt = 0;
+    rep(i, N) {
+        cin >> a;
+        a_i[i][0] = a;
+        a_i[i][1] = i + 1;
+        if (a == i + 1) {
+            same_num_cnt++;
+        }
+        sort(all(a_i[i]));
+    }
+    sort(all(a_i));
+    ll ans = (same_num_cnt * (same_num_cnt - 1)) / 2;
+    rep(i, N - 1) {
+        if (a_i[i][0] == a_i[i + 1][0] && a_i[i][1] == a_i[i + 1][1]) {
+            ans++;
+        }
+    }
+    cout << ans << endl;
 }
