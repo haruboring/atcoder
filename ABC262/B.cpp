c278590df5fd1941e6694894a2bacb56d50c42e0
haruboting <raiu_hk@icloud.com>
2022-10-30 17:22:33 +0900

    solve

--- ABC262/B.cpp
+++ ABC262/B.cpp
@@ -6,4 +6,35 @@ using namespace std;
 using ll = long long;
 
 int main() {
+    int N, M;
+    cin >> N >> M;
+    vector<vector<int>> UV(N, vector<int>(0));
+    int U, V;
+    rep(i, M) {
+        cin >> U >> V;
+        UV[U - 1].push_back(V - 1);
+        UV[V - 1].push_back(U - 1);
+    }
+    int ans = 0;
+    rep(i, N) {
+        // 頂点→i
+        rep(j, UV[i].size()) {
+            // 頂点→UV[i][j]
+            if (UV[i][j] == i) {
+                continue;
+            }
+            rep(k, UV[UV[i][j]].size()) {
+                // 頂点UV[UV[i][j]][k]
+                if (UV[UV[i][j]][k] == UV[i][j]) {
+                    continue;
+                }
+                rep(l, UV[UV[UV[i][j]][k]].size()) {
+                    if (UV[UV[UV[i][j]][k]][l] == i) {
+                        ans++;
+                    }
+                }
+            }
+        }
+    }
+    cout << ans / 6 << endl;
 }
