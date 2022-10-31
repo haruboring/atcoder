21e7b6c9d7894ba669a82e8ef02884f47bc6bf56
haruboting <raiu_hk@icloud.com>
2022-10-31 12:42:08 +0900

    solve

--- ABC259/C.cpp
+++ ABC259/C.cpp
@@ -6,4 +6,30 @@ using namespace std;
 using ll = long long;
 
 int main() {
+    string S, T;
+    cin >> S >> T;
+    int T_size = T.size(), S_size = S.size();
+    int S_index = 0;
+    rep(i, T_size) {
+        if (S_index > S_size) {
+            cout << "No" << endl;
+            return 0;
+        }
+        if (T[i] == S[S_index]) {
+            S_index++;
+        } else {
+            if (S_index >= 1) {
+                if (S[S_index - 2] == S[S_index - 1] && S[S_index - 1] == T[i]) {
+                    continue;
+                } else {
+                    cout << "No" << endl;
+                    return 0;
+                }
+            } else {
+                cout << "No" << endl;
+                return 0;
+            }
+        }
+    }
+    cout << "Yes" << endl;
 }
