4874c4f80738ecf12e15f8818adb89f1ba121e63
haruboting <raiu_hk@icloud.com>
2022-11-02 02:00:17 +0900

    solve

--- ABC252/C.cpp
+++ ABC252/C.cpp
@@ -6,4 +6,36 @@ using namespace std;
 using ll = long long;
 
 int main() {
+    int N;
+    cin >> N;
+    vector<string> S(N);
+    rep(i, N) {
+        cin >> S[i];
+    }
+    vector<int> sec(0);
+    rep(i, 10) {
+        multiset<int> pos;
+        int time = 0;
+        rep(j, N) {
+            rep(k, 10) {
+                if (S[j][k] == char(i + '0')) {
+                    pos.insert(k);
+                    break;
+                }
+            }
+        }
+        rep(k, 999) {
+            if (pos.size() == 0) {
+                sec.push_back(time);
+                break;
+            }
+            if (pos.find(k % 10) == pos.end()) {
+                continue;
+            }
+            time = 10 * (k / 10) + *rbegin(pos);
+            pos.erase(pos.find(k % 10));
+        }
+    }
+    sort(all(sec));
+    cout << sec[0] << endl;
 }
