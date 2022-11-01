c7fc8c37a84861f4845c007f5aa74d232eba5ec5
haruboting <raiu_hk@icloud.com>
2022-11-01 20:04:15 +0900

    solve

--- ABC252/A.cpp
+++ ABC252/A.cpp
@@ -6,4 +6,7 @@ using namespace std;
 using ll = long long;
 
 int main() {
+    int N;
+    cin >> N;
+    cout << char(N) << endl;
 }
--- ABC252/B.cpp
+++ ABC252/B.cpp
@@ -6,4 +6,28 @@ using namespace std;
 using ll = long long;
 
 int main() {
+    int N, K;
+    cin >> N >> K;
+    vector<pair<int, int>> A_i(N);
+    rep(i, N) {
+        cin >> A_i[i].first;
+        A_i[i].second = i + 1;
+    }
+    vector<int> B(K);
+    rep(i, K) {
+        cin >> B[i];
+    }
+    sort(all(A_i));
+    reverse(all(A_i));
+    rep(i, N) {
+        if (A_i[i].first == A_i[0].first) {
+            rep(j, K) {
+                if (A_i[i].second == B[j]) {
+                    cout << "Yes" << endl;
+                    return 0;
+                }
+            }
+        }
+    }
+    cout << "No" << endl;
 }
