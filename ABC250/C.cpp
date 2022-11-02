8c366436ea6cf4bf36019791b731128b73ced9a2
haruboting <raiu_hk@icloud.com>
2022-11-02 19:01:35 +0900

    solve

--- ABC250/C.cpp
+++ ABC250/C.cpp
@@ -6,4 +6,42 @@ using namespace std;
 using ll = long long;
 
 int main() {
+    int N, Q;
+    cin >> N >> Q;
+    vector<int> x(Q);
+    rep(i, Q) {
+        cin >> x[i];
+    }
+    // value -> index
+    map<int, int> v_i, i_v;
+    rep(i, N) {
+        v_i[i + 1] = i;
+        i_v[i] = i + 1;
+    }
+    rep(i, Q) {
+        if (v_i[x[i]] != N - 1) {
+            int rvalue = i_v[v_i[x[i]] + 1];
+            int lvalue = x[i];
+            int rindex = v_i[rvalue];
+            int lindex = v_i[lvalue];
+            i_v[lindex] = rvalue;
+            i_v[rindex] = lvalue;
+            v_i[lvalue] = rindex;
+            v_i[rvalue] = lindex;
+        }
+        else{
+            int rvalue = x[i];
+            int lvalue = i_v[v_i[x[i]] - 1];
+            int rindex = v_i[rvalue];
+            int lindex = v_i[lvalue];
+            i_v[lindex] = rvalue;
+            i_v[rindex] = lvalue;
+            v_i[lvalue] = rindex;
+            v_i[rvalue] = lindex;
+        }
+    }
+    rep(i, N) {
+        cout << i_v[i] << " ";
+    }
+    cout << endl;
 }
