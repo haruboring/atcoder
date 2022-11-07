6164dd593e183a7b5039729fac1ba0aeda81f361
haruboting <raiu_hk@icloud.com>
2022-11-08 01:26:27 +0900

    solve

---
+++ 競技プログラミングの鉄則/A09.cpp
@@ -0,0 +1,40 @@
+#include "bits/stdc++.h"
+#define all(v) v.begin(), v.end()
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define repp(i, m, n) for (int i = m; i < (int)(n); i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+    int H, W, N;
+    cin >> H >> W >> N;
+    vector<vector<int>> Z(H + 2, vector<int>(W + 2));
+    rep(i, N) {
+        int A, B, C, D;
+        cin >> A >> B >> C >> D;
+        Z[A][B]++;
+        Z[A][D + 1]--;
+        Z[C + 1][B]--;
+        Z[C + 1][D + 1]++;
+    }
+    rep(i, H + 1) {
+        int cnt = 0;
+        rep(j, W + 1) {
+            cnt += Z[i][j];
+            Z[i][j] = cnt;
+        }
+    }
+    rep(i, W + 1) {
+        int cnt = 0;
+        rep(j, H + 1) {
+            cnt += Z[j][i];
+            Z[j][i] = cnt;
+        }
+    }
+    rep(i, H) {
+        rep(j, W) {
+            cout << Z[i + 1][j + 1] << " ";
+        }
+        cout << endl;
+    }
+}
---
+++ 競技プログラミングの鉄則/B08.cpp
@@ -0,0 +1,47 @@
+#include "bits/stdc++.h"
+#define all(v) v.begin(), v.end()
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define repp(i, m, n) for (int i = m; i < (int)(n); i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+    int N;
+    cin >> N;
+    vector<int> X(N), Y(N);
+    rep(i, N) {
+        cin >> X[i] >> Y[i];
+    }
+    int Q;
+    cin >> Q;
+    vector<map<char, int>> ad(Q);
+    rep(i, Q) {
+        int a, b, c, d;
+        cin >> a >> b >> c >> d;
+        ad[i]['a'] = a;
+        ad[i]['b'] = b;
+        ad[i]['c'] = c;
+        ad[i]['d'] = d;
+    }
+    vector<vector<int>> place(1500 + 1, vector<int>(1500 + 1));
+    rep(i, N) {
+        place[X[i]][Y[i]]++;
+    }
+    rep(i, 1500 + 1) {
+        int cnt = 0;
+        rep(j, 1500 + 1) {
+            cnt += place[i][j];
+            place[i][j] = cnt;
+        }
+    }
+    rep(i, 1500 + 1) {
+        int cnt = 0;
+        rep(j, 1500 + 1) {
+            cnt += place[j][i];
+            place[j][i] = cnt;
+        }
+    }
+    rep(i, Q) {
+        cout << place[ad[i]['c']][ad[i]['d']] - place[ad[i]['c']][ad[i]['b'] - 1] - place[ad[i]['a'] - 1][ad[i]['d']] + place[ad[i]['a'] - 1][ad[i]['b'] - 1] << endl;
+    }
+}
