89fcaf1ca4add96ad322f9bab472835112fc8180
haruboting <raiu_hk@icloud.com>
2022-11-08 12:09:52 +0900

    solve

---
+++ 競技プログラミングの鉄則/A11.cpp
@@ -0,0 +1,30 @@
+#include "bits/stdc++.h"
+#define all(v) v.begin(), v.end()
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define repp(i, m, n) for (int i = m; i < (int)(n); i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+    int N, X;
+    cin >> N >> X;
+    vector<int> A(N);
+    rep(i, N) {
+        cin >> A[i];
+    }
+    int mid = N / 2, left = 0, right = N - 1;
+    while (left != right) {
+        if (A[mid] < X) {
+            left = mid + 1;
+            mid = (left + right) / 2;
+        } else if (A[mid] == X) {
+            cout << mid + 1 << endl;
+            return 0;
+        } else {
+            right = mid - 1;
+            mid = (left + right) / 2;
+        }
+    }
+
+    cout << left + 1 << endl;
+}
---
+++ 競技プログラミングの鉄則/B11.cpp
@@ -0,0 +1,24 @@
+#include "bits/stdc++.h"
+#define all(v) v.begin(), v.end()
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define repp(i, m, n) for (int i = m; i < (int)(n); i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+    int N;
+    cin >> N;
+    vector<int> A(N);
+    rep(i, N) {
+        cin >> A[i];
+    }
+    sort(all(A));
+    int Q;
+    cin >> Q;
+    rep(i, Q) {
+        int X;
+        cin >> X;
+        int pos = lower_bound(A.begin(), A.end(), X) - A.begin();
+        cout << pos << endl;
+    }
+}
