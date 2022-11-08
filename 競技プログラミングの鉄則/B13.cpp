44b88d88e0a99e40b021b2649fd337bb23842b35
haruboting <raiu_hk@icloud.com>
2022-11-08 17:30:35 +0900

    solve

---
+++ 競技プログラミングの鉄則/B13.cpp
@@ -0,0 +1,46 @@
+#include "bits/stdc++.h"
+#define all(v) v.begin(), v.end()
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define repp(i, m, n) for (int i = m; i < (int)(n); i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+    ll N, K;
+    cin >> N >> K;
+    vector<ll> A(N + 2), LA(N + 2), RA(N + 2);
+    rep(i, N) {
+        cin >> A[i + 1];
+    }
+    ll sum = 0;
+    rep(i, N + 1) {
+        sum += A[i + 1];
+        LA[i + 1] = sum;
+    }
+    sum = 0;
+    rep(i, N + 1) {
+        sum += A[N - i];
+        RA[N - i] = sum;
+    }
+    vector<ll> R(N + 1);
+    R[0] = 1;
+    repp(i, 1, N + 1) {
+        ll ri = max(R[i - 1], (ll)i);
+        while (1) {
+            if (ri > N) {
+                break;
+            }
+            if (sum - LA[i - 1] - RA[ri + 1] <= K) {
+                ri++;
+            } else {
+                break;
+            }
+        }
+        R[i] = ri;
+    }
+    ll ans = 0;
+    rep(i, N) {
+        ans += (R[i + 1] - (i + 1));
+    }
+    cout << ans << endl;
+}
