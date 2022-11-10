c47ced8a5bb54ac81c07488e65f3915c081c8938
haruboting <raiu_hk@icloud.com>
2022-11-10 14:55:15 +0900

    solve

---
+++ 競技プログラミングの鉄則/A14.cpp
@@ -0,0 +1,39 @@
+#include "bits/stdc++.h"
+#define all(v) v.begin(), v.end()
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define repp(i, m, n) for (int i = m; i < (int)(n); i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+    int N, K;
+    cin >> N >> K;
+    vector<int> A(N), B(N), C(N), D(N);
+    rep(i, N) {
+        cin >> A[i];
+    }
+    rep(i, N) {
+        cin >> B[i];
+    }
+    rep(i, N) {
+        cin >> C[i];
+    }
+    rep(i, N) {
+        cin >> D[i];
+    }
+    vector<int> AB(0), CD(0);
+    rep(i, N) {
+        rep(j, N) {
+            AB.push_back(A[i] + B[j]);
+            CD.push_back(C[i] + D[j]);
+        }
+    }
+    sort(all(CD));
+    rep(i, N * N) {
+        if (binary_search(CD.begin(), CD.end(), K - AB[i])) {
+            cout << "Yes" << endl;
+            return 0;
+        }
+    }
+    cout << "No" << endl;
+}
---
+++ 競技プログラミングの鉄則/B14.cpp
@@ -0,0 +1,45 @@
+#include "bits/stdc++.h"
+#define all(v) v.begin(), v.end()
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define repp(i, m, n) for (int i = m; i < (int)(n); i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+    int N, K;
+    cin >> N >> K;
+    vector<int> A(N);
+    rep(i, N) {
+        cin >> A[i];
+    }
+    int hlf = N / 2;
+    vector<int> first_hlf(0), second_hlf(0);
+    rep(i, 1 << hlf) {
+        bitset<16> S(i);
+        int sum = 0;
+        rep(j, hlf) {
+            if (S.test(j)) {
+                sum += A[j];
+            }
+        }
+        first_hlf.push_back(sum);
+    }
+    rep(i, 1 << (N - hlf)) {
+        bitset<16> S(i);
+        int sum = 0;
+        rep(j, N - hlf) {
+            if (S.test(j)) {
+                sum += A[hlf + j];
+            }
+        }
+        second_hlf.push_back(sum);
+    }
+    sort(all(second_hlf));
+    rep(i, first_hlf.size()) {
+        if (binary_search(second_hlf.begin(), second_hlf.end(), K - first_hlf[i])) {
+            cout << "Yes" << endl;
+            return 0;
+        }
+    }
+    cout << "No" << endl;
+}
