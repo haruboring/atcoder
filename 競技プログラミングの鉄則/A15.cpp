988e63ff465cd6f93593226a443bad6120c6ba1f
haruboting <raiu_hk@icloud.com>
2022-11-10 15:40:24 +0900

    solve

---
+++ 競技プログラミングの鉄則/A15.cpp
@@ -0,0 +1,31 @@
+#include "bits/stdc++.h"
+#define all(v) v.begin(), v.end()
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define repp(i, m, n) for (int i = m; i < (int)(n); i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+    int N;
+    cin >> N;
+    vector<int> A(N);
+    rep(i, N) {
+        cin >> A[i];
+    }
+    set<int> contain;
+    vector<int> not_duplicated(0), ans(N);
+    rep(i, N) {
+        if (contain.count(A[i])) {
+            continue;
+        } else {
+            contain.insert(A[i]);
+            not_duplicated.push_back(A[i]);
+        }
+    }
+    sort(all(not_duplicated));
+    rep(i, N) {
+        int pos = lower_bound(all(not_duplicated), A[i]) - not_duplicated.begin();
+        cout << pos + 1 << " ";
+    }
+    cout << endl;
+}
