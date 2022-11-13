cc62f8c1a376b6b81dc6fc96b94670a0ece4c438
haruboting <raiu_hk@icloud.com>
2022-11-14 00:06:47 +0900

    solve

---
+++ 競技プログラミングの鉄則/B18.cpp
@@ -0,0 +1,48 @@
+#include "bits/stdc++.h"
+#define all(v) v.begin(), v.end()
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define repp(i, m, n) for (int i = m; i < (int)(n); i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+    int N, S;
+    cin >> N >> S;
+    vector<int> A(N);
+    rep(i, N) {
+        cin >> A[i];
+    }
+    vector<vector<bool>> dp(N + 1, vector<bool>(N * 10000 + 10));
+    rep(i, N + 1) {
+        rep(j, N * 10000 + 10) {
+            dp[i][j] = false;
+        }
+    }
+    dp[0][0] = true;
+    repp(i, 1, N + 1) {
+        rep(j, N * 10000 + 10) {
+            if (dp[i - 1][j] == true) {
+                dp[i][j] = true;
+                dp[i][j + A[i - 1]] = true;
+            }
+        }
+    }
+    if (dp[N][S]) {
+        vector<int> ans(0);
+        int pos = S;
+        rep(i, N) {
+            if (pos - A[N - 1 - i] >= 0 && dp[N - 1 - i][pos - A[N - 1 - i]]) {
+                ans.push_back(N - i);
+                pos -= A[N - 1 - i];
+            }
+        }
+        sort(all(ans));
+        cout << ans.size() << endl;
+        rep(i, ans.size()) {
+            cout << ans[i] << " ";
+        }
+        cout << endl;
+    } else {
+        cout << -1 << endl;
+    }
+}
