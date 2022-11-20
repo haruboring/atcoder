6cdf576fded5a663dd29a523aa53f54626ee5867
haruboting <raiu_hk@icloud.com>
2022-11-20 21:18:19 +0900

    solve

---
+++ 競技プログラミングの鉄則/A22.cpp
@@ -0,0 +1,28 @@
+#include "bits/stdc++.h"
+#define all(v) v.begin(), v.end()
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define repp(i, m, n) for (int i = m; i < (int)(n); i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+    int N;
+    cin >> N;
+    vector<ll> A(N), B(N);
+    rep(i, N - 1) {
+        cin >> A[i + 1];
+    }
+    rep(i, N - 1) {
+        cin >> B[i + 1];
+    }
+    vector<ll> ans(N + 1);
+    repp(i, 1, N) {
+        if(ans[i]!=0 || i == 1){
+            ans[A[i]] = max(ans[i] + 100, ans[A[i]]);
+        }
+        if(ans[i]!=0 || i == 1){
+            ans[B[i]] = max(ans[i] + 150, ans[B[i]]);
+        }
+    }
+    cout << ans[N] << endl;
+}
