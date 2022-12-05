3831c06bc7ecff6fecf6d3da406eab71402bbb61
haruboting <raiu_hk@icloud.com>
2022-12-05 16:27:25 +0900

    solve

--- ABC280/B.cpp
+++ ABC280/B.cpp
@@ -6,4 +6,20 @@ using namespace std;
 using ll = long long;
 
 int main() {
+    int N;
+    cin >> N;
+    vector<ll> S(N);
+    rep(i, N) {
+        cin >> S[i];
+    }
+    vector<ll> A(0);
+    A.push_back(S[0]);
+    rep(i, N - 1) {
+        A.push_back(S[i+1]-S[i]);
+
+    }
+    rep(i, N) {
+        cout << A[i] << " ";
+    }
+    cout << endl;
 }
