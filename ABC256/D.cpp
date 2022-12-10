d894d724b359091c01f4fb2f0619f2cf372dd95e
haruboting <raiu_hk@icloud.com>
2022-12-10 20:37:35 +0900

    solve

--- ABC256/D.cpp
+++ ABC256/D.cpp
@@ -6,4 +6,25 @@ using namespace std;
 using ll = long long;
 
 int main() {
+    int N;
+    cin >> N;
+    vector<int> A(200000 + 100);
+    rep(i, N) {
+        int L, R;
+        cin >> L >> R;
+        A[L]++;
+        A[R]--;
+    }
+    int cnt = 0;
+    bool b = false;
+    rep(i, 200000 + 100) {
+        cnt += A[i];
+        if (b == false && cnt > 0) {
+            cout << i << " ";
+            b = true;
+        } else if (cnt < 1 && b) {
+            cout << i << endl;
+            b = false;
+        }
+    }
 }
