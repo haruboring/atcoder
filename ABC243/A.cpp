0ae16b9a1bdbff8dd86eb554beaa1180a9f8240a
haruboting <raiu_hk@icloud.com>
2022-12-11 21:03:43 +0900

    solve

--- ABC243/A.cpp
+++ ABC243/A.cpp
@@ -6,4 +6,22 @@ using namespace std;
 using ll = long long;
 
 int main() {
+    int V;
+    cin >> V;
+    vector<int> use(3);
+    rep(i, 3) {
+        cin >> use[i];
+    }
+    int i = -1;
+    while (V >= 0) {
+        i++;
+        V -= use[i % 3];
+    }
+    if (i%3 == 0) {
+        cout << "F" << endl;
+    } else if (i%3 == 1) {
+        cout << "M" << endl;
+    } else {
+        cout << "T" << endl;
+    }
 }
