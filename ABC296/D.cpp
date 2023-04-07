ec46ca31399ec0a29cfa6ef76a0d4c2cb2089665
haruboting <raiu_hk@icloud.com>
2023-04-07 18:24:24 +0900

    solve

--- ABC296/D.cpp
+++ ABC296/D.cpp
@@ -1,9 +1,26 @@
 #include "bits/stdc++.h"
 #define all(v) v.begin(), v.end()
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
-#define repp(i, m, n) for (int i = m; i < (int)(n); i++)
+#define repp(i, m, n) for (ll i = m; i < (ll)(n); i++)
 using namespace std;
 using ll = long long;
 
 int main() {
+    ll N, M;
+    cin >> N >> M;
+    ll ans = 9223372036854775807;
+    repp(i, 1, sqrt(M) + 2) {
+        ll k = M / i;
+        if (M % i != 0) {
+            k++;
+        }
+        if (k <= N && i <= N) {
+            ans = min(ans, k * i);
+        }
+    }
+    if (ans == 9223372036854775807) {
+        cout << -1 << endl;
+    } else {
+        cout << ans << endl;
+    }
 }
