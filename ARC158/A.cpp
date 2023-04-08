4a5a69e6d211cd5f81ce085875e3cf5e7cfb5b08
haruboting <raiu_hk@icloud.com>
2023-04-09 01:59:40 +0900

    Add ARC158 folder

---
+++ ARC158/A.cpp
@@ -0,0 +1,23 @@
+#include "bits/stdc++.h"
+#define all(v) v.begin(), v.end()
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define repp(i, m, n) for (int i = m; i < (int)(n); i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+    int T;
+    cin >> T;
+    // 0, 6, 6
+    // 0, 0, 0
+    // 0, 2, 4
+    // a(024), b(042), c(204), d(240), e(420), f(402)
+    // 000→0
+    // 002→
+
+    rep(i, T) {
+        ll x1, x2, x3;
+        cin >> x1 >> x2 >> x3;
+        
+    }
+}
