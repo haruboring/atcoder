abadc5d2eac42649f05758779fe4601e97c4f606
haruboting <raiu_hk@icloud.com>
2023-04-15 20:15:24 +0900

    solve

--- ABC295/D.cpp
+++ ABC295/D.cpp
@@ -6,4 +6,32 @@ using namespace std;
 using ll = long long;
 
 int main() {
+    string S;
+    cin >> S;
+    set<char> all = {};
+    rep(i, S.size()) {
+        if (all.count(S[i])) {
+            all.erase(S[i]);
+        } else {
+            all.insert(S[i]);
+        }
+    }
+    set<char> l = all, r = all;
+    map<set<char>, set<int>> m;
+    m[all].insert(0);
+    rep(i, S.size()) {
+        if (l.count(S[i])) {
+            l.erase(S[i]);
+        } else {
+            l.insert(S[i]);
+        }
+        m[l].insert(i + 1);
+    }
+
+    ll ans = 0;
+    for (auto a : m) {
+        ll s = a.second.size();
+        ans += (s) * (s - 1) / 2;
+    }
+    cout << ans << endl;
 }
