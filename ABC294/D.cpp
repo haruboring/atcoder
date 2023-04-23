ee7ef7fedda0717cb38e763c9b18fad6c309d242
haruboting <raiu_hk@icloud.com>
2023-04-24 04:55:16 +0900

    Solve

--- ABC294/D.cpp
+++ ABC294/D.cpp
@@ -6,4 +6,26 @@ using namespace std;
 using ll = long long;
 
 int main() {
+    int N, Q;
+    cin >> N >> Q;
+    set<int> call, go, uncall;
+    rep(i, N) {
+        uncall.insert(i + 1);
+    }
+    rep(i, Q) {
+        int flag;
+        cin >> flag;
+        if (flag == 1) {
+            int calling_number = *begin(uncall);
+            uncall.erase(calling_number);
+            call.insert(calling_number);
+        } else if (flag == 2) {
+            int x;
+            cin >> x;
+            call.erase(x);
+            go.insert(x);
+        } else {
+            cout << int(*begin(call)) << endl;
+        }
+    }
 }
