82b2a736bc88a4b91b9f5be59b8d247228852c75
haruboting <raiu_hk@icloud.com>
2023-05-01 05:27:12 +0900

    solve

--- ABC300/B.cpp
+++ ABC300/B.cpp
@@ -6,4 +6,34 @@ using namespace std;
 using ll = long long;
 
 int main() {
+    int H, W;
+    cin >> H >> W;
+    vector<string> A(H), B(H);
+    rep(i, H) {
+        cin >> A[i];
+    }
+    rep(i, H) {
+        cin >> B[i];
+    }
+    rep(s_cnt, H) {
+        rep(t_cnt, W) {
+            bool b = true;
+            rep(i, H) {
+                rep(j, W) {
+                    if (A[(i + s_cnt) % H][(j + t_cnt) % W] != B[i][j]) {
+                        b = false;
+                        break;
+                    }
+                }
+                if (!b) {
+                    break;
+                }
+            }
+            if (b) {
+                cout << "Yes" << endl;
+                return 0;
+            }
+        }
+    }
+    cout << "No" << endl;
 }
