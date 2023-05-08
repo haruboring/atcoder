10db389c49521ba5cf7eca9abaead14b6943a40e
haruboting <raiu_hk@icloud.com>
2023-05-08 23:13:12 +0900

    Solve

--- ABC114/A.cpp
+++ ABC114/A.cpp
@@ -6,4 +6,11 @@ using namespace std;
 using ll = long long;
 
 int main() {
+    int X;
+    cin >> X;
+    if (X == 3 || X == 5 || X == 7) {
+        cout << "YES" << endl;
+    } else {
+        cout << "NO" << endl;
+    }
 }
