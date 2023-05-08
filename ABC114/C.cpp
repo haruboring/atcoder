1a93f8387c606cde0530a589383528854db50cc9
haruboting <raiu_hk@icloud.com>
2023-05-09 00:25:45 +0900

    Solve

--- ABC114/C.cpp
+++ ABC114/C.cpp
@@ -6,4 +6,24 @@ using namespace std;
 using ll = long long;
 
 int main() {
+    int N;
+    cin >> N;
+    int ans = 0;
+    vector<string> hitigosan = {"7", "5", "3"};
+    repp(keta, 1, 9 + 1) {
+        rep(i, pow(3, keta)) {
+            string s = "";
+            int copy_i = i;
+            set<int> appear;
+            rep(_, keta) {
+                s += hitigosan[copy_i % 3];
+                appear.insert(copy_i % 3);
+                copy_i /= 3;
+            }
+            if (stoll(s) <= N && appear.size() == 3) {
+                ans++;
+            }
+        }
+    }
+    cout << ans << endl;
 }
