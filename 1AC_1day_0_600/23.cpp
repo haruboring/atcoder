693fa7ae84bde2bc6d51f67db4c10c98f77071e3
haruboting <raiu_hk@icloud.com>
2023-05-23 11:06:12 +0900

    Solve

---
+++ 1AC_1day_0_600/23.cpp
@@ -0,0 +1,12 @@
+#include "bits/stdc++.h"
+#define all(v) v.begin(), v.end()
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define repp(i, m, n) for (int i = m; i < (int)(n); i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+    int M1, D1, M2, D2;
+    cin >> M1 >> D1 >> M2 >> D2;
+    cout << (M1 != M2) << endl;
+}
