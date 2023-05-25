d679fb56848a66172cbacee53cd5bc05dda02222
haruboting <raiu_hk@icloud.com>
2023-05-25 15:51:18 +0900

    solve

---
+++ 1AC_1day_600_1400/15.cpp
@@ -0,0 +1,40 @@
+#include "bits/stdc++.h"
+#define all(v) v.begin(), v.end()
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define repp(i, m, n) for (int i = m; i < (int)(n); i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+    int N, M;
+    cin >> N >> M;
+    vector<vector<ll>> xyz(N, vector<ll>(3));
+    rep(i, N) {
+        rep(j, 3) {
+            cin >> xyz[i][j];
+        }
+    }
+
+    ll ans = -1e18;
+
+    rep(i, (1 << 3)) {
+        bitset<3> s(i);
+        vector<ll> sum(N);
+        rep(j, N) {
+            rep(k, 3) {
+                if (s.test(k)) {
+                    sum[j] += xyz[j][k];
+                } else {
+                    sum[j] -= xyz[j][k];
+                }
+            }
+        }
+        sort(all(sum));
+        ll tmp = 0;
+        rep(j, M) tmp += sum[N - 1 - j];
+
+        ans = max(ans, tmp);
+    }
+
+    cout << ans << endl;
+}
