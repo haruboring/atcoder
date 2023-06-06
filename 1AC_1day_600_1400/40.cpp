65b58904f9cb4d1f53a4b603b6c221ae367969e5
haruboting <raiu_hk@icloud.com>
2023-06-06 23:16:21 +0900

    solve

---
+++ 1AC_1day_600_1400/40.cpp
@@ -0,0 +1,34 @@
+#include "bits/stdc++.h"
+#define all(v) v.begin(), v.end()
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define repp(i, m, n) for (int i = m; i < (int)(n); i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+    ll N, K, M, R;
+    cin >> N >> K >> M >> R;
+    vector<ll> S(N - 1);
+    rep(i, N - 1) cin >> S[i];
+
+    sort(all(S), greater<ll>());
+
+    ll sum = 0;
+    rep(i, K - 1) {
+        sum += S[i];
+    }
+    ll need = R * K - sum;
+    if (K < N && need <= S[K - 1]) {
+        cout << 0 << endl;
+        return 0;
+    }
+    if (K == N && need <= 0) {
+        cout << 0 << endl;
+        return 0;
+    }
+    if (need > M) {
+        cout << -1 << endl;
+        return 0;
+    }
+    cout << need << endl;
+}
