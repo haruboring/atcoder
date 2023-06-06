c24bb70e9d9854d9ea5fe6cb98f7781885982a7b
haruboting <raiu_hk@icloud.com>
2023-06-06 23:17:27 +0900

    solve

---
+++ 1AC_1day_600_1400/42.cpp
@@ -0,0 +1,42 @@
+#include "bits/stdc++.h"
+#define all(v) v.begin(), v.end()
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define repp(i, m, n) for (int i = m; i < (int)(n); i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+    int N, M;
+    cin >> N >> M;
+    vector<string> b(N);
+    rep(i, N) cin >> b[i];
+
+    vector<vector<int>> B(N, vector<int>(M));
+    rep(i, N) {
+        rep(j, M) {
+            B[i][j] = b[i][j] - '0';
+        }
+    }
+
+    vector<vector<int>> ans(N, vector<int>(M));
+    // 外枠から攻めろ
+    repp(i, 0, N - 2) {
+        repp(j, 1, M - 1) {
+            int cnt = B[i][j];
+            if (cnt > 0) {
+                ans[i + 1][j] += cnt;
+                B[i][j] -= cnt;
+                B[i + 1][j + 1] -= cnt;
+                B[i + 1][j - 1] -= cnt;
+                B[i + 2][j] -= cnt;
+            }
+        }
+    }
+
+    rep(i, N) {
+        rep(j, M) {
+            cout << ans[i][j];
+        }
+        cout << endl;
+    }
+}
