0f330a68b356176466319d46e1354a30e949065d
haruboting <raiu_hk@icloud.com>
2023-06-10 19:54:34 +0900

    add: debug

--- ABC/B.cpp
+++ ABC/B.cpp
@@ -2,6 +2,7 @@
 #define all(v) v.begin(), v.end()
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
 #define repp(i, m, n) for (int i = m; i < (int)(n); i++)
+#define debug(x) cerr << #x << ": " << x << endl
 using namespace std;
 using ll = long long;
 
