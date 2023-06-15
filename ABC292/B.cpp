da1c9fadf682d3a658dc52729220822a7243b247
haruboting <raiu_hk@icloud.com>
2023-06-15 14:17:14 +0900

    solve

--- ABC292/B.cpp
+++ ABC292/B.cpp
@@ -6,4 +6,20 @@ using namespace std;
 using ll = long long;
 
 int main() {
+    int N, Q;
+    cin >> N >> Q;
+    vector<ll> A(N);
+    rep(i, Q) {
+        int flag, x;
+        cin >> flag >> x;
+        if (flag == 3) {
+            if (A[x - 1] < 2) {
+                cout << "No" << endl;
+            } else {
+                cout << "Yes" << endl;
+            }
+        } else {
+            A[x - 1] += flag;
+        }
+    }
 }
