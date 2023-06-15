ca4dddc71c07af594834900c2fd3ea580d77a0e6
haruboting <raiu_hk@icloud.com>
2023-06-15 14:02:48 +0900

    solve

--- ABC292/F.cpp
+++ ABC292/F.cpp
@@ -6,4 +6,15 @@ using namespace std;
 using ll = long long;
 
 int main() {
+    double A, B;
+    cin >> A >> B;
+
+    // A<=B
+    if (A > B) swap(A, B);
+
+    if (3 * B * B >= 4 * A * A) {
+        cout << setprecision(20) << 2 * A / sqrt(3) << endl;
+        return 0;
+    }
+    cout << setprecision(20) << B / cos(atan(2 * A / B - sqrt(3))) << endl;
 }
