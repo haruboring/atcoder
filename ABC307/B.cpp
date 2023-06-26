1bdd9b4020c1698ee2055317b0269847b0836edd
haruboting <raiu_hk@icloud.com>
2023-06-26 11:14:35 +0900

    solve

--- ABC307/B.cpp
+++ ABC307/B.cpp
@@ -7,4 +7,22 @@ using namespace std;
 using ll = long long;
 
 int main() {
+    int N;
+    cin >> N;
+    vector<string> S(N);
+    rep(i, N) cin >> S[i];
+
+    rep(i, N) {
+        rep(j, N) {
+            if (i == j) continue;
+            string concated = S[i] + S[j];
+            string r_concated = concated;
+            reverse(all(r_concated));
+            if (concated == r_concated) {
+                cout << "Yes" << endl;
+                return 0;
+            }
+        }
+    }
+    cout << "No" << endl;
 }
