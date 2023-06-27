afebb4a6ee22fefd1147d90a738ddf9ec677eaff
haruboting <raiu_hk@icloud.com>
2023-06-27 23:21:14 +0900

    solve

--- ABC307/C.cpp
+++ ABC307/C.cpp
@@ -7,4 +7,34 @@ using namespace std;
 using ll = long long;
 
 int main() {
+    int N;
+    cin >> N;
+    string S;
+    cin >> S;
+
+    vector<int> dels(N);
+    set<int> left;
+    map<int,int> l2r;
+    rep(i, N) {
+        if (S[i] == '(') {
+            left.insert(i);
+        }
+        if (S[i] == ')') {
+            if (left.empty()) continue;
+            int l = *left.rbegin();
+            debug(l);
+            left.erase(l);
+            l2r[l] = i;
+
+            for (int j = l; j <= i; j++) {
+                if (dels[j] == 1) j = l2r[j];
+                dels[j] = 1;
+            }
+        }
+    }
+
+    rep(i, N) {
+        if (!dels[i]) cout << S[i];
+    }
+    cout << endl;
 }
