1ceddd0d42ca99538e0bd83b44b49337929e83bf
haruboting <raiu_hk@icloud.com>
2023-06-29 23:33:04 +0900

    solve

--- ABC307/E.cpp
+++ ABC307/E.cpp
@@ -7,4 +7,22 @@ using namespace std;
 using ll = long long;
 
 int main() {
+    ll N, M;
+    cin >> N >> M;
+
+    ll MOD = 998244353;
+
+    vector<vector<ll>> dp(2, vector<ll>(N));
+    // 1番目と一緒ではない
+    dp[0][0] = 0;
+    // 1番目と一緒
+    dp[1][0] = M;
+    rep(i, N - 1) {
+        dp[0][i + 1] = dp[0][i] * (M - 2) + dp[1][i] * (M - 1);
+        dp[1][i + 1] = dp[0][i];
+        dp[0][i + 1] %= MOD;
+        dp[1][i + 1] %= MOD;
+    }
+
+    cout << dp[0][N - 1] << endl;
 }
