d51a84782e35d172da960fdcbb84fbb7f6b70883
haruboting <raiu_hk@icloud.com>
2023-07-02 06:03:44 +0900

    solve

--- ARC161/A.cpp
+++ ARC161/A.cpp
@@ -7,4 +7,26 @@ using namespace std;
 using ll = long long;
 
 int main() {
+    int N;
+    cin >> N;
+    vector<int> A(N);
+    rep(i, N) cin >> A[i];
+
+    sort(all(A));
+    vector<int> B(N);
+    rep(i, N / 2 + 1) {
+        B[i * 2] = A[i];
+    }
+    rep(i, N / 2) {
+        B[i * 2 + 1] = A[i + N / 2 + 1];
+    }
+
+    for (int i = 1; i < N; i += 2) {
+        if (B[i - 1] >= B[i] || B[i] <= B[i + 1]) {
+            cout << "No" << endl;
+            return 0;
+        }
+    }
+
+    cout << "Yes" << endl;
 }
--- ARC161/B.cpp
+++ ARC161/B.cpp
@@ -7,4 +7,25 @@ using namespace std;
 using ll = long long;
 
 int main() {
+    ll T;
+    cin >> T;
+    vector<ll> tmp(0);
+    rep(i, 63 - 2) {
+        repp(j, i + 1, 63 - 1) {
+            repp(k, j + 1, 63) {
+                tmp.push_back((1LL << i) + (1LL << j) + (1LL << k));
+            }
+        }
+    }
+    sort(all(tmp));
+    rep(_, T) {
+        ll N;
+        cin >> N;
+        int idx = upper_bound(all(tmp), N) - tmp.begin();
+        if (idx == 0) {
+            cout << -1 << endl;
+        } else {
+            cout << tmp[idx - 1] << endl;
+        }
+    }
 }
