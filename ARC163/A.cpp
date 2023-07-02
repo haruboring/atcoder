e6502049690daed42441afd219731ffd6186a67b
haruboting <raiu_hk@icloud.com>
2023-07-03 01:19:21 +0900

    solve

--- ARC163/A.cpp
+++ ARC163/A.cpp
@@ -7,4 +7,48 @@ using namespace std;
 using ll = long long;
 
 int main() {
+    int T;
+    cin >> T;
+
+    rep(i, T) {
+        int size;
+        cin >> size;
+        string s;
+        cin >> s;
+
+        vector<string> ans(0);
+        string rest = "";
+        while (s.size() > 0) {
+            string st = s.substr(0, 1);
+            string anst = s.substr(1, s.size() - 1);
+            while (anst.size() > 0) {
+                if (rest < st && st < anst) {
+                    if (i == 3) {
+                        debug(st);
+                    }
+                    break;
+                }
+                st += anst.substr(0, 1);
+                anst = anst.substr(1, anst.size() - 1);
+            }
+            ans.push_back(st);
+            rest = st;
+            s = s.substr(st.size(), s.size() - st.size());
+        }
+        if (ans.size() < 2) {
+            cout << "No" << endl;
+        } else {
+            bool b = true;
+            rep(i, ans.size() - 1) {
+                if (ans[i] >= ans[i + 1]) {
+                    cout << "No" << endl;
+                    b = false;
+                    break;
+                }
+            }
+            if (b) {
+                cout << "Yes" << endl;
+            }
+        }
+    }
 }
--- ARC163/B.cpp
+++ ARC163/B.cpp
@@ -1,10 +1,29 @@
 #include "bits/stdc++.h"
 #define all(v) v.begin(), v.end()
-#define rep(i, n) for (int i = 0; i < (int)(n); i++)
-#define repp(i, m, n) for (int i = m; i < (int)(n); i++)
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define repp(i, m, n) for (ll i = m; i < (ll)(n); i++)
 #define debug(x) cerr << #x << ": " << x << endl
 using namespace std;
 using ll = long long;
 
 int main() {
+    ll N, M;
+    cin >> N >> M;
+    vector<ll> A(N);
+    rep(i, N) cin >> A[i];
+
+    vector<ll> B(N - 2);
+    rep(i, N - 2) B[i] = A[i + 2];
+
+    ll low = A[0], up = A[1];
+    sort(all(B));
+    ll ans = 1e18;
+    rep(i, N - 1 - M) {
+        ll l = B[i];
+        ll u = B[i + M - 1];
+        ll need_low = max(0LL, low - l);
+        ll need_up = max(0LL, u - up);
+        ans = min(ans, need_low + need_up);
+    }
+    cout << ans << endl;
 }
