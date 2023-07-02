f5156eea4ea87b12944d5449bb25133fc0a8e648
haruboting <raiu_hk@icloud.com>
2023-07-02 17:00:03 +0900

    solve

--- ARC161/D.cpp
+++ ARC161/D.cpp
@@ -7,4 +7,19 @@ using namespace std;
 using ll = long long;
 
 int main() {
+    int N, D;
+    cin >> N >> D;
+
+    // そもそも、グラフが存在しない
+    if (N - 1 < 2 * D) {
+        cout << "No" << endl;
+        return 0;
+    }
+
+    cout << "Yes" << endl;
+    rep(i, N) {
+        rep(j, D) {
+            cout << i + 1 << " " << (i + j + 1) % N + 1 << endl;
+        }
+    }
 }
