05847314374dffa34095fec94ab05b1c772b5ad6
haruboting <raiu_hk@icloud.com>
2023-07-03 09:34:55 +0900

    solve

--- ARC163/C.cpp
+++ ARC163/C.cpp
@@ -7,4 +7,47 @@ using namespace std;
 using ll = long long;
 
 int main() {
+    int T;
+    cin >> T;
+    rep(_, T) {
+        int N;
+        cin >> N;
+
+        if (N == 1) {
+            cout << "Yes" << endl;
+            cout << 1 << endl;
+            continue;
+        }
+        if (N == 2) {
+            cout << "No" << endl;
+            continue;
+        }
+
+        vector<int> A(N);
+        set<int> s;
+        rep(i, N - 1) {
+            A[i] = (i + 1) * (i + 2);
+            s.insert(A[i]);
+        }
+        if (!s.count(N)) {
+            cout << "Yes" << endl;
+            A[N - 1] = N;
+            rep(i, N) {
+                cout << A[i] << " ";
+            }
+            cout << endl;
+            continue;
+        }
+        vector<int> B(N);
+        B[0] = 2;
+        rep(i, N - 2) {
+            B[i + 1] = 2 * (i + 1) * (i + 2);
+        }
+        B[N - 1] = 2 * (N - 1);
+        cout << "Yes" << endl;
+        rep(i, N) {
+            cout << B[i] << " ";
+        }
+        cout << endl;
+    }
 }
