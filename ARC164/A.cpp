d067f61feb0fce63285dc699883b50d402d02ad7
haruboting <raiu_hk@icloud.com>
2023-07-10 07:12:17 +0900

    solve

--- ARC164/A.cpp
+++ ARC164/A.cpp
@@ -7,4 +7,35 @@ using namespace std;
 using ll = long long;
 
 int main() {
+    int T;
+    cin >> T;
+    rep(_, T) {
+        ll N, K;
+        cin >> N >> K;
+
+        ll ma = N;
+        ll max_n = 1;
+        while (max_n <= N) {
+            max_n *= 3;
+        }
+        if (N != 1) max_n /= 3;
+
+        ll mi = 0;
+        while (N > 0) {
+            if (N >= max_n) {
+                mi += N / max_n;
+                N -= max_n * (N / max_n);
+            }
+            max_n /= 3;
+        }
+
+        debug(0);
+        debug(mi);
+        debug(ma);
+        if (mi <= K && K <= ma && (K - mi) % 2 == 0) {
+            cout << "Yes" << endl;
+        } else {
+            cout << "No" << endl;
+        }
+    }
 }
