0ffd7e0b01e59b7542620906d291572659548a27
haruboting <raiu_hk@icloud.com>
2023-07-11 19:12:18 +0900

    solve

--- ARC164/C.cpp
+++ ARC164/C.cpp
@@ -7,4 +7,27 @@ using namespace std;
 using ll = long long;
 
 int main() {
+    int N;
+    cin >> N;
+    vector<int> A(N), B(N);
+    rep(i, N) cin >> A[i] >> B[i];
+
+    multiset<pair<int, int>> cards;
+    rep(i, N) {
+        cards.insert({A[i] - B[i], A[i]});
+    }
+
+    ll ans = 0;
+    while (!cards.empty()) {
+        auto [d1, a1] = *cards.rbegin();
+        int b1 = a1 - d1;
+        cards.erase(cards.find({d1, a1}));
+        cards.insert({-d1, b1});
+
+        auto [d2, a2] = *cards.rbegin();
+        cards.erase(cards.find({d2, a2}));
+        ans += a2;
+    }
+
+    cout << ans << endl;
 }
