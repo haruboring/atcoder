0d98840d7f13ecef1c30bdd51e4a4e1ca0fe6e69
haruboting <raiu_hk@icloud.com>
2023-07-15 03:23:45 +0900

    solve

--- ALPC/J.cpp
+++ ALPC/J.cpp
@@ -1,3 +1,4 @@
+#include "atcoder/all"
 #include "bits/stdc++.h"
 #define all(v) v.begin(), v.end()
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
@@ -6,5 +7,28 @@
 using namespace std;
 using ll = long long;
 
+int op(int a, int b) { return max(a, b); }
+int e() { return 0; }
+int target;
+bool comp(int a) { return a < target; }
+
 int main() {
+    int N, Q;
+    cin >> N >> Q;
+    vector<int> A(N);
+    rep(i, N) cin >> A[i];
+
+    atcoder::segtree<int, op, e> seg(A);
+    rep(_, N) {
+        int t, x, y;
+        cin >> t >> x >> y;
+        if (t == 1) {
+            seg.set(x - 1, y);
+        } else if (t == 2) {
+            cout << seg.prod(x - 1, y) << endl;
+        } else {
+            target = y;
+            cout << seg.max_right<comp>(x - 1) + 1 << endl;
+        }
+    }
 }
