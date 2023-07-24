6be2fbc33177930a426f46a08e42e23da608e8b3
haruboting <raiu_hk@icloud.com>
2023-07-25 02:52:12 +0900

    solve

--- ABC311/D.cpp
+++ ABC311/D.cpp
@@ -8,4 +8,48 @@ using namespace std;
 using ll = long long;
 
 int main() {
+    int N, M;
+    cin >> N >> M;
+    vector<string> S(N + 2);
+    S[0] = string(M + 2, '#');
+    rep(i, N) {
+        string s;
+        cin >> s;
+        S[i + 1] = "#" + s + "#";
+    }
+    S[N + 1] = string(M + 2, '#');
+
+    set<pair<int, int>> st;
+    st.insert({2, 2});
+    queue<pair<int, int>> q;
+    q.push({2, 2});
+    set<pair<int, int>> already_stop;
+    already_stop.insert({2, 2});
+
+    vector<vector<int>> move = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+    while (!q.empty()) {
+        auto [x, y] = q.front();
+        q.pop();
+        rep(i, 4) {
+            int dx = move[i][0];
+            int dy = move[i][1];
+            int nx = x;
+            int ny = y;
+            while (S[nx + dx][ny + dy] == '.') {
+                st.insert({nx, ny});
+                nx += dx;
+                ny += dy;
+            }
+            if (already_stop.count({nx, ny}) == 0) {
+                q.push({nx, ny});
+                already_stop.insert({nx, ny});
+            }
+        }
+    }
+
+    // for(auto [a, b] : already_stop) {
+    //     cout << "a: " << a << " b: " << b << endl;
+    // }
+
+    cout << st.size() << endl;
 }
