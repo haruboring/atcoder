a468b7194808509e360e04138ad0e27612e77b7c
haruboting <raiu_hk@icloud.com>
2023-08-05 20:30:52 +0900

    solve

--- ABC262/D.cpp
+++ ABC262/D.cpp
@@ -6,4 +6,28 @@ using namespace std;
 using ll = long long;
 
 int main() {
+    int N;
+    cin >> N;
+    vector<ll> A(N);
+    rep(i, N) cin >> A[i];
+
+    ll BASE = 998244353;
+    ll ans = 0;
+    for (int i = 1; i <= N; i++) {
+        vector<vector<vector<ll>>> dp(N + 1, vector<vector<ll>>(i + 2, vector<ll>(i, 0)));
+        dp[0][0][0] = 1;
+        for (int j = 0; j < N; j++) {
+            for (int k = 0; k <= min(i, j); k++) {
+                for (int l = 0; l < i; l++) {
+                    dp[j + 1][k][l] += dp[j][k][l];
+                    dp[j + 1][k][l] %= BASE;
+                    dp[j + 1][k + 1][(l + A[j]) % i] += dp[j][k][l];
+                    dp[j + 1][k + 1][(l + A[j]) % i] %= BASE;
+                }
+            }
+        }
+        ans += dp[N][i][0];
+        ans %= BASE;
+    }
+    cout << ans << endl;
 }
