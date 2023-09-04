29a0e0d08f55ce459e0a8acd1fb53c4bc22eb584
haruboting <raiu_hk@icloud.com>
2023-09-04 22:46:29 +0900

    solve

--- ABC032/C.cpp
+++ ABC032/C.cpp
@@ -8,4 +8,33 @@
 using namespace std;
 
 signed main() {
+    int N, K;
+    cin >> N >> K;
+    vector<int> S(N);
+    rep(i, N) cin >> S[i];
+
+    rep(i, N) {
+        if (S[i] == 0) {
+            cout << N << endl;
+            return 0;
+        }
+    }
+
+    int ans = 0;
+
+    int tmp = 1;
+    int r = 0;
+    rep(l, N) {
+        while (r < N && tmp * S[r] <= K) {
+            tmp *= S[r];
+            r++;
+        }
+        ans = max(ans, r - l);
+        if (l == r)
+            r++;
+        else
+            tmp /= S[l];
+    }
+
+    cout << ans << endl;
 }
