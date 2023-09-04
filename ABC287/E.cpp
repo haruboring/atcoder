912f9b49f7f4da04e330d52a6ab2a95a9efd9b33
haruboting <raiu_hk@icloud.com>
2023-09-05 00:32:30 +0900

    solve

--- ABC287/E.cpp
+++ ABC287/E.cpp
@@ -6,4 +6,40 @@ using namespace std;
 using ll = long long;
 
 int main() {
+    int N;
+    cin >> N;
+    vector<string> S(N);
+    rep(i, N) cin >> S[i];
+
+    vector<string> T = S;
+
+    sort(all(S));
+
+    rep(i, N) {
+        string s = T[i];
+
+        int itr = lower_bound(all(S), s) - S.begin();
+
+        int ans = 0;
+        if (itr + 1 < N) {
+            string t = S[itr + 1];
+            int cnt = 0;
+            rep(j, s.size()) {
+                if (s[j] != t[j]) break;
+                cnt++;
+            }
+            ans = max(ans, cnt);
+        }
+        if (itr - 1 >= 0) {
+            string t = S[itr - 1];
+            int cnt = 0;
+            rep(j, s.size()) {
+                if (s[j] != t[j]) break;
+                cnt++;
+            }
+            ans = max(ans, cnt);
+        }
+
+        cout << ans << endl;
+    }
 }
