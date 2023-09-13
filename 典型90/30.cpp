cd8868aea2715493b82c23ee9b1aefb6b95e4bf5
haruboting <raiu_hk@icloud.com>
2023-09-14 04:57:01 +0900

    solve

---
+++ 典型90/30.cpp
@@ -0,0 +1,77 @@
+#include "atcoder/all"
+#include "bits/stdc++.h"
+#define all(v) v.begin(), v.end()
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define repp(i, m, n) for (ll i = m; i < (ll)(n); i++)
+#define debug(x) cerr << #x << ": " << x << endl
+using namespace std;
+using ll = long long;
+
+// エラトステネスの篩
+struct Eratosthenes {
+    // テーブル
+    vector<bool> isprime;
+
+    // 整数 i を割り切る最小の素数
+    vector<int> minfactor;
+
+    // コンストラクタで篩を回す
+    Eratosthenes(int N) : isprime(N + 1, true),
+                          minfactor(N + 1, -1) {
+        // 1 は予めふるい落としておく
+        isprime[1] = false;
+        minfactor[1] = 1;
+
+        // 篩
+        for (int p = 2; p <= N; ++p) {
+            // すでに合成数であるものはスキップする
+            if (!isprime[p]) continue;
+
+            // p についての情報更新
+            minfactor[p] = p;
+
+            // p 以外の p の倍数から素数ラベルを剥奪
+            for (int q = p * 2; q <= N; q += p) {
+                // q は合成数なのでふるい落とす
+                isprime[q] = false;
+
+                // q は p で割り切れる旨を更新
+                if (minfactor[q] == -1) minfactor[q] = p;
+            }
+        }
+    }
+
+    // 高速素因数分解
+    // pair (素因子, 指数) の vector を返す
+    vector<pair<int, int>> factorize(int n) {
+        vector<pair<int, int>> res;
+        while (n > 1) {
+            int p = minfactor[n];
+            int exp = 0;
+
+            // n で割り切れる限り割る
+            while (minfactor[n] == p) {
+                n /= p;
+                ++exp;
+            }
+            res.emplace_back(p, exp);
+        }
+        return res;
+    }
+};
+
+signed main() {
+    int N, K;
+    cin >> N >> K;
+
+    Eratosthenes er(N+1);
+    int ans = 0;
+    for (int i = 2; i <= N; i++) {
+        auto pf = er.factorize(i);
+        if (pf.size() >= K) {
+            ans++;
+        }
+    }
+
+    cout << ans << endl;
+}
