d13b32fce2161d3645d2bc9233f7fa2c1781c55c
haruboting <raiu_hk@icloud.com>
2023-09-14 04:41:36 +0900

    solve

--- ABC319/E.cpp
+++ ABC319/E.cpp
@@ -8,4 +8,36 @@
 using namespace std;
 
 signed main() {
+    int N, X, Y;
+    cin >> N >> X >> Y;
+    vector<int> P(N - 1), T(N - 1);
+    rep(i, N - 1) cin >> P[i] >> T[i];
+    int Q;
+    cin >> Q;
+    vector<int> q(Q);
+    rep(i, Q) cin >> q[i];
+
+    vector<int> cost(3 * 5 * 7 * 8);
+    repp(i, 1, 3 * 5 * 7 * 8 + 1) {
+        int tmp = i;
+
+        for (int j = 0; j < N - 1; j++) {
+            if (tmp % P[j] == 0) {
+                tmp += T[j];
+            } else {
+                tmp += (P[j] - tmp % P[j]) + T[j];
+            }
+        }
+
+        cost[i % (3 * 5 * 7 * 8)] = tmp - i;
+    }
+
+    rep(i, Q) {
+        int ans = q[i];
+        ans += X;
+        ans += cost[ans % (3 * 5 * 7 * 8)];
+        ans += Y;
+
+        cout << ans << endl;
+    }
 }
