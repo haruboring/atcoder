6bc7fa2867d96283d3a224e0e998aee982e36537
haruboting <raiu_hk@icloud.com>
2023-09-17 04:37:27 +0900

    solve

--- ABC267/D.cpp
+++ ABC267/D.cpp
@@ -8,4 +8,22 @@
 using namespace std;
 
 signed main() {
+    int N, M;
+    cin >> N >> M;
+    vector<int> A(N);
+    rep(i, N) {
+        cin >> A[i];
+    }
+
+    vector<vector<int>> dp(N + 1, vector<int>(M + 1, -1e18));
+    dp[0][0] = 0;
+    rep(i, N) {
+        rep(j, M + 1) {
+            if (dp[i][j] == -1e18) continue;
+            dp[i + 1][j] = max(dp[i + 1][j], dp[i][j]);
+            if (j < M) dp[i + 1][j + 1] = max(dp[i + 1][j + 1], dp[i][j] + (j + 1) * A[i]);
+        }
+    }
+
+    cout << dp[N][M] << endl;
 }
