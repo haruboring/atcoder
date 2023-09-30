d4fd6db2d6b2b47b4cc4cb5e29a6701ebdc06df2
haruboting <raiu_hk@icloud.com>
2023-09-30 20:00:46 +0900

    solve

--- ABC264/C.cpp
+++ ABC264/C.cpp
@@ -8,4 +8,51 @@
 using namespace std;
 
 signed main() {
+    int H1, W1;
+    cin >> H1 >> W1;
+    vector<vector<int>> A(H1, vector<int>(W1));
+    rep(i, H1) rep(j, W1) cin >> A[i][j];
+    int H2, W2;
+    cin >> H2 >> W2;
+    vector<vector<int>> B(H2, vector<int>(W2));
+    rep(i, H2) rep(j, W2) cin >> B[i][j];
+
+    vector<vector<int>> select_H(0), select_W(0);
+
+    rep(i, (1LL << H1)) {
+        bitset<10> bs(i);
+        vector<int> tmp;
+        rep(j, H1) {
+            if (bs[j]) tmp.push_back(j);
+        }
+        if (tmp.size() == H2) select_H.push_back(tmp);
+    }
+
+    rep(i, (1LL << W1)) {
+        bitset<10> bs(i);
+        vector<int> tmp;
+        rep(j, W1) {
+            if (bs[j]) tmp.push_back(j);
+        }
+        if (tmp.size() == W2) select_W.push_back(tmp);
+    }
+
+    rep(i, select_H.size()) {
+        sort(all(select_H[i]));
+        rep(j, select_W.size()) {
+            sort(all(select_W[j]));
+
+            vector<vector<int>> C(H2, vector<int>(W2));
+            rep(k, H2) rep(l, W2) {
+                C[k][l] = A[select_H[i][k]][select_W[j][l]];
+            }
+
+            if (C == B) {
+                cout << "Yes" << endl;
+                return 0;
+            }
+        }
+    }
+
+    cout << "No" << endl;
 }
--- ABC264/E.cpp
+++ ABC264/E.cpp
@@ -7,5 +7,89 @@
 #define debug(x) cerr << #x << ": " << x << endl
 using namespace std;
 
+vector<int> par(200000 + 100);
+
+void init(int n) {
+    rep(i, n) par[i] = i;
+}
+
+int root(int x) {
+    if (par[x] == x) return x;
+    return par[x] = root(par[x]);
+}
+
+int unite(int x, int y) {
+    int rx = root(x);
+    int ry = root(y);
+    if (ry < rx) swap(rx, ry);
+    if (rx == ry) {
+        return 0;
+    } else {
+        return par[rx] = ry;
+    }
+}
+
 signed main() {
+    int N, M, E;
+    cin >> N >> M >> E;
+    vector<int> U(E), V(E);
+    rep(i, E) {
+        cin >> U[i] >> V[i];
+        U[i]--;
+        V[i]--;
+    }
+    int Q;
+    cin >> Q;
+    vector<int> X(Q);
+    set<int> st;
+    rep(i, Q) {
+        cin >> X[i];
+        X[i]--;
+        st.insert(X[i]);
+    }
+
+    init(N + M);
+
+    map<int, int> cnt;
+    rep(i, E) {
+        if (st.count(i)) continue;
+        unite(U[i], V[i]);
+    }
+    rep(i, N + M) {
+        cnt[root(i)]++;
+    }
+
+    vector<int> ans(Q);
+    ans[0] = 0;
+    repp(i, N, N + M) ans[0] += cnt[i] - 1;
+    debug(ans[0]);
+    reverse(all(X));
+
+    rep(i, Q - 1) {
+        ans[i + 1] = ans[i];
+        int e = X[i];
+        int ru = root(U[e]);
+        int rv = root(V[e]);
+
+        unite(U[e], V[e]);
+        if (ru == rv) continue;
+
+        int tmp = -1;
+        if (ru > rv) {
+            cnt[ru] += cnt[rv];
+            tmp = cnt[rv];
+            cnt[rv] = 0;
+        } else {
+            cnt[rv] += cnt[ru];
+            tmp = cnt[ru];
+            cnt[ru] = 0;
+        }
+
+        if (ru >= N && rv >= N) continue;
+        if (ru < N && rv < N) continue;
+        ans[i + 1] += tmp;
+    }
+
+    reverse(all(ans));
+    rep(i, Q) cout << ans[i] << endl;
 }
