987892c91b7b1431b833b05114a80366d058e940
haruboting <raiu_hk@icloud.com>
2023-10-14 02:21:31 +0900

    solve

--- ABC204/D.cpp
+++ ABC204/D.cpp
@@ -8,4 +8,30 @@
 using namespace std;
 
 signed main() {
+    int N;
+    cin >> N;
+    vector<int> T(N);
+    rep(i, N) cin >> T[i];
+
+    int total = 0;
+    rep(i, N) total += T[i];
+
+    vector<vector<int>> dp(N + 1, vector<int>(1e5 + 100, -1));
+
+    dp[0][0] = 0;
+    rep(i, N) {
+        rep(j, 1e5 + 100) {
+            if (dp[i][j] == -1) continue;
+
+            dp[i + 1][j] = 1;
+            dp[i + 1][j + T[i]] = 1;
+        }
+    }
+
+    int ans = 1e9;
+    rep(i, 1e5 + 100) {
+        if (dp[N][i] == 1) ans = min(ans, max(i, total - i));
+    }
+
+    cout << ans << endl;
 }
