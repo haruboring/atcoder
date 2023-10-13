8dd7b6daa9d8009ae3e37368b242715a63b121fb
haruboting <raiu_hk@icloud.com>
2023-10-14 02:25:38 +0900

    solve

--- ABC202/C.cpp
+++ ABC202/C.cpp
@@ -1,13 +1,13 @@
 #include "atcoder/all"
 #include "bits/stdc++.h"
+#define int long long
 #define all(v) v.begin(), v.end()
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
 #define repp(i, m, n) for (int i = m; i < (int)(n); i++)
 #define debug(x) cerr << #x << ": " << x << endl
 using namespace std;
-using ll = long long;
 
-int main() {
+signed main() {
     int N;
     cin >> N;
     vector<int> A(N + 1), B(N + 1), C(N + 1);
@@ -15,16 +15,19 @@ int main() {
     rep(i, N) cin >> B.at(i + 1);
     rep(i, N) cin >> C.at(i + 1);
 
-    int cnt = 0;
-
-    map<int, int> mp;
-
+    map<int, int> mpb;
     rep(i, N) {
-        mp[B.at(C.at(i + 1))]++;
+        mpb[B.at(C.at(i + 1))]++;
     }
 
+    map<int, int> mpa;
     rep(i, N) {
-        cnt += mp[A.at(i + 1)];
+        mpa[A.at(i + 1)]++;
+    }
+
+    int cnt = 0;
+    rep(i, N + 1) {
+        cnt += mpa[i] * mpb[i];
     }
 
     cout << cnt << endl;
