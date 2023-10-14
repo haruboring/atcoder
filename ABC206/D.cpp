e7113a511544b20c7d3dbc5ecc935e9c4086c9bf
haruboting <raiu_hk@icloud.com>
2023-10-14 20:54:36 +0900

    solve

--- ABC206/D.cpp
+++ ABC206/D.cpp
@@ -7,5 +7,45 @@
 #define debug(x) cerr << #x << ": " << x << endl
 using namespace std;
 
+vector<int> par(200000 + 100);
+
+void init(int n) {
+    rep(i, n) par[i] = i;
+}
+
+int root(int x) {
+    if (par[x] == x) return x;
+    return par[x] = root(par[x]);
+}
+
+int unite(int x, int y) {
+    int rx = root(x);
+    int ry = root(y);
+    if (ry < rx) swap(rx, ry);
+    if (rx == ry) {
+        return 0;
+    } else {
+        return par[rx] = ry;
+    }
+}
+
 signed main() {
+    int N;
+    cin >> N;
+    vector<int> A(N);
+    rep(i, N) cin >> A[i];
+
+    init(2 * 1e5 + 1);
+    rep(i, N / 2) {
+        if (A[i] != A[N - 1 - i]) {
+            unite(A[i], A[N - 1 - i]);
+        }
+    }
+
+    set<int> rt;
+    rep(i, 2 * 1e5 + 1) {
+        rt.insert(root(i));
+    }
+
+    cout << 2 * 1e5 + 1 - rt.size() << endl;
 }
