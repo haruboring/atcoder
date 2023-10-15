f743258c8d5767124a13414a930eec5b30aa8f1c
haruboting <raiu_hk@icloud.com>
2023-10-15 16:29:25 +0900

    solve

--- ABC205/D.cpp
+++ ABC205/D.cpp
@@ -8,4 +8,29 @@
 using namespace std;
 
 signed main() {
+    int N, Q;
+    cin >> N >> Q;
+    vector<int> A(N);
+    rep(i, N) cin >> A[i];
+
+    sort(all(A));
+
+    rep(_, Q) {
+        int K;
+        cin >> K;
+
+        int l = 0, r = 2e18;
+        while (r - l > 1) {
+            int mid = (l + r) / 2;
+            int itr = upper_bound(all(A), mid) - A.begin();
+
+            if (mid - itr < K) {
+                l = mid;
+            } else {
+                r = mid;
+            }
+        }
+
+        cout << r << endl;
+    }
 }
