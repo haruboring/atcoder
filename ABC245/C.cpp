d3c340a38876ad1714b88f739b2db0f17edcf33a
haruboting <raiu_hk@icloud.com>
2023-10-23 23:12:06 +0900

    solve

--- ABC245/C.cpp
+++ ABC245/C.cpp
@@ -8,9 +8,27 @@
 using namespace std;
 
 signed main() {
-    int N;
-    cin >> N;
-    string S;
-    cin >> S;
-    cout << S[N - 1] << endl;
+    int N, K;
+    cin >> N >> K;
+    vector<vector<int>> AB(N, vector<int>(2));
+    rep(i, N) cin >> AB[i][0];
+    rep(i, N) cin >> AB[i][1];
+
+    vector<vector<bool>> ok(N, vector<bool>(2, false));
+    ok[0][0] = true;
+    ok[0][1] = true;
+
+    rep(i, N - 1) {
+        rep(j, 2) {
+            if (!ok[i][j]) continue;
+            rep(k, 2) {
+                if (abs(AB[i][j] - AB[i + 1][k]) <= K) ok[i + 1][k] = true;
+            }
+        }
+    }
+
+    if (ok[N - 1][0] || ok[N - 1][1])
+        cout << "Yes" << endl;
+    else
+        cout << "No" << endl;
 }
