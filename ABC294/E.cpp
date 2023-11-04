392661f7f1f3c36123d3d8d0944b4f64b7083f4f
haruboting <raiu_hk@icloud.com>
2023-11-04 20:44:10 +0900

    solve

--- ABC294/E.cpp
+++ ABC294/E.cpp
@@ -8,4 +8,37 @@
 using namespace std;
 
 signed main() {
+    int L, N1, N2;
+    cin >> L >> N1 >> N2;
+    vector<vector<int>> vl1(2, vector<int>(N1)), vl2(2, vector<int>(N2));
+    rep(i, N1) cin >> vl1[0][i] >> vl1[1][i];
+    rep(i, N2) cin >> vl2[0][i] >> vl2[1][i];
+
+    rep(i, N1 - 1) vl1[1][i + 1] += vl1[1][i];
+    rep(i, N2 - 1) vl2[1][i + 1] += vl2[1][i];
+
+    int ans = 0;
+    int idx1 = 0, idx2 = 0;
+
+    int tl = 0;
+    while (tl != L) {
+        int v1 = vl1[0][idx1], v2 = vl2[0][idx2];
+        int l1 = vl1[1][idx1], l2 = vl2[1][idx2];
+
+        if (l1 <= l2) {
+            if (v1 == v2) {
+                ans += l1 - tl;
+            }
+            tl = l1;
+            if (idx1 != N1 - 1) idx1++;
+        } else {
+            if (v1 == v2) {
+                ans += l2 - tl;
+            }
+            tl = l2;
+            if (idx2 != N2 - 1) idx2++;
+        }
+    }
+
+    cout << ans << endl;
 }
--- ABC326/E.cpp
+++ ABC326/E.cpp
@@ -7,5 +7,49 @@
 #define debug(x) cerr << #x << ": " << x << endl
 using namespace std;
 
+int llpow(int a, int n) {
+    int res = 1;
+    while (n > 0) {
+        if (n & 1) res = res * a;
+        a = a * a;
+        n >>= 1;
+    }
+    return res;
+}
+
+// a^n mod を計算する
+int modpow(int a, int n, int mod) {
+    int res = 1;
+    while (n > 0) {
+        if (n & 1) res = res * a % mod;
+        a = a * a % mod;
+        n >>= 1;
+    }
+    return res;
+}
+
+// a^{-1} mod を計算する
+int modinv(int a, int mod) {
+    return modpow(a, mod - 2, mod);
+}
+
 signed main() {
+    int N;
+    cin >> N;
+    vector<int> A(N);
+    rep(i, N) cin >> A[i];
+
+    int MOD = 998244353;
+    int inv_N = modinv(N, MOD);
+
+    int p = inv_N, ans = 0;
+
+    rep(i, N) {
+        ans = (ans + p * A[i]) % MOD;
+
+        p *= 1 + inv_N;
+        p %= MOD;
+    }
+
+    cout << ans << endl;
 }
