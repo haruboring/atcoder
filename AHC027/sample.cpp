427a6bf5eb306659050ed791aebb2385805c9342
haruboting <raiu_hk@icloud.com>
2023-12-03 04:59:45 +0900

    add: base

---
+++ AHC027/sample.cpp
@@ -0,0 +1,75 @@
+#include "bits/stdc++.h"
+#include "fstream"
+#define all(v) v.begin(), v.end()
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define repp(i, m, n) for (int i = m; i < (int)(n); i++)
+#define debug(x) cerr << #x << ": " << x << endl
+using namespace std;
+
+vector<pair<int, int>> dij = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+string dir = "RDLU";
+
+string ans = "";
+
+void dfs(int i, int j, int N, vector<string>& h, vector<string>& v, vector<vector<int>>& d, vector<vector<bool>>& visited) {
+    visited[i][j] = true;
+    for (int k = 0; k < 4; k++) {
+        int di = dij[k].first;
+        int dj = dij[k].second;
+        int i2 = i + di;
+        int j2 = j + dj;
+        if (0 <= i2 && i2 < N && 0 <= j2 && j2 < N && !visited[i2][j2]) {
+            if ((di == 0 && v[i][min(j, j2)] == '0') || (dj == 0 && h[min(i, i2)][j] == '0')) {
+                ans += dir[k];
+                dfs(i2, j2, N, h, v, d, visited);
+                ans += dir[(k + 2) % 4];
+            }
+        }
+    }
+}
+
+int main(int argc, char* argv[]) {
+    ifstream fin(argv[1]);
+    if (!fin) {
+        cerr << "can not open " << argv[1] << endl;
+        return 1;
+    }
+
+    ofstream fout("tools/tmp_out.txt");
+
+    int N;
+    fin >> N;
+    vector<string> h(N - 1);
+    vector<string> v(N);
+    vector<vector<int>> d(N, vector<int>(N));
+    vector<vector<bool>> visited(N, vector<bool>(N, false));
+
+    rep(i, N - 1) fin >> h[i];
+    rep(i, N) fin >> v[i];
+    rep(i, N) rep(j, N) fin >> d[i][j];
+
+    dfs(0, 0, N, h, v, d, visited);
+
+    fout << ans << endl;
+
+    return 0;
+}
+
+// // 提出用
+// signed main() {
+//     int N;
+//     cin >> N;
+//     vector<string> h(N - 1);
+//     vector<string> v(N);
+//     vector<vector<int>> d(N, vector<int>(N));
+//     vector<vector<bool>> visited(N, vector<bool>(N, false));
+
+//     rep(i, N - 1) cin >> h[i];
+//     rep(i, N) cin >> v[i];
+//     rep(i, N) rep(j, N) cin >> d[i][j];
+
+//     dfs(0, 0, N, h, v, d, visited);
+//     cout << ans << endl;
+
+//     return 0;
+// }
---
+++ AHC027/test.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# 引数チェック
+if [ "$#" -ne 1 ]; then
+    echo "使用方法: $0 [ファイル名]"
+    exit 1
+fi
+
+source_file=$1
+
+sum=0
+
+# コンパイル
+g++-13 -std=c++17 -I../.. $source_file -o tools/a.out
+
+# 0000.txtから0100.txtまで繰り返し実行
+for i in $(seq -f "%04g" 0 100); do
+    input_file_name="${i}.txt"
+    echo ""
+    echo "Processing file: ${input_file_name}"
+
+    # プログラムを実行
+    ./tools/a.out "tools/in/${input_file_name}"
+
+    # cargo run コマンドの出力を取得し、echo する
+    output=$(cd tools && cargo run -r --bin vis in/${input_file_name} tmp_out.txt)
+    echo "Output for ${input_file_name}: $output"
+
+    # 数値のみを抽出して足し合わせる
+    num=$(echo $output | grep -o '[0-9]\+')
+    sum=$((sum + num))
+    echo "Current Total: $sum"
+done
+
+# 最終合計を sum_score.txt に追加
+echo "$source_file: $sum" >> sum_score.txt
