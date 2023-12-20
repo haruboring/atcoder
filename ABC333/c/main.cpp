88c74a1e7ca5b540e9ea6eb89e6d59a4ba254c8e
haruboting <raiu_hk@icloud.com>
2023-12-21 07:24:20 +0900

    add

--- ABC333/c/main.cpp
+++ ABC333/c/main.cpp
@@ -8,4 +8,34 @@
 using namespace std;
 
 signed main() {
+    int N;
+    cin >> N;
+
+    vector<int> repunit(0);
+
+    int tmp = 0;
+    rep(i, 16) {
+        tmp = tmp * 10 + 1;
+        repunit.push_back(tmp);
+    }
+
+    vector<int> wa(0);
+    rep(i, 16) {
+        rep(j, 16) {
+            rep(k, 16) {
+                wa.push_back(repunit[i] + repunit[j] + repunit[k]);
+            }
+        }
+    }
+
+    set<int> s(all(wa));
+
+    int cnt = 0;
+    for (auto x : s) {
+        cnt++;
+        if (cnt == N) {
+            cout << x << endl;
+            return 0;
+        }
+    }
 }
