ec814e267d7db2bb0059add7f365420a30564744
haruboting <raiu_hk@icloud.com>
2024-01-14 13:53:51 +0900

    solve

--- ABC333/e/main.cpp
+++ ABC333/e/main.cpp
@@ -8,4 +8,49 @@
 using namespace std;
 
 signed main() {
+    int N;
+    cin >> N;
+    vector<int> t(N), x(N);
+    rep(i, N) cin >> t[i] >> x[i];
+
+    map<int, int> mp;
+
+    rep(i, N) {
+        if (t[i] == 1) {
+            mp[x[i]]++;
+        } else {
+            mp[x[i]]--;
+            if (mp[x[i]] < 0) {
+                cout << -1 << endl;
+                return 0;
+            }
+        }
+    }
+
+    mp.clear();
+    vector<int> ans(0);
+    int max_K = 0;
+    int sum = 0;
+    rep(i, N) {
+        if (t[N - 1 - i] == 2) {
+            mp[x[N - 1 - i]]--;
+            sum++;
+        } else {
+            if (mp[x[N - 1 - i]] < 0) {
+                mp[x[N - 1 - i]]++;
+                sum--;
+                ans.push_back(1);
+            } else {
+                ans.push_back(0);
+            }
+        }
+
+        max_K = max(max_K, sum);
+    }
+
+    cout << max_K << endl;
+    rep(i, ans.size()) {
+        cout << ans[ans.size() - 1 - i] << " ";
+    }
+    cout << endl;
 }
