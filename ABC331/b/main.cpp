c19121619f0021db70276f945d032e834840ab1f
haruboting <raiu_hk@icloud.com>
2024-01-14 14:08:19 +0900

    solve

--- ABC331/b/main.cpp
+++ ABC331/b/main.cpp
@@ -8,4 +8,19 @@
 using namespace std;
 
 signed main() {
+    int N, S, M, L;
+    cin >> N >> S >> M >> L;
+
+    int ans = 1e9;
+    rep(i, 20) {
+        rep(j, 20) {
+            rep(k, 20) {
+                if (i * 6 + j * 8 + k * 12 >= N) {
+                    ans = min(ans, i * S + j * M + k * L);
+                }
+            }
+        }
+    }
+
+    cout << ans << endl;
 }
