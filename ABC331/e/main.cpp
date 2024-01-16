c8e1a457f2d1b2bbe25a1d764c0ab7ce8bf5ac06
haruboting <raiu_hk@icloud.com>
2024-01-16 22:47:45 +0900

    solve

--- ABC331/e/main.cpp
+++ ABC331/e/main.cpp
@@ -8,4 +8,47 @@
 using namespace std;
 
 signed main() {
+    int N, M, L;
+    cin >> N >> M >> L;
+    vector<int> a(N);
+    rep(i, N) cin >> a[i];
+    vector<int> b(M);
+    rep(i, M) cin >> b[i];
+    vector<int> aa = a, bb = b;
+    vector<int> c(L), d(L);
+    rep(i, L) cin >> c[i] >> d[i];
+
+    sort(all(a));
+    sort(all(b));
+
+    int left = 0, right = 1e18;
+    while (right - left > 1) {
+        int mid = (left + right) / 2;
+        int cnt = 0;
+        rep(i, N) {
+            cnt += M - (lower_bound(all(b), mid - a[i]) - b.begin());
+        }
+        if (cnt > L) {
+            left = mid;
+        } else {
+            right = mid;
+        }
+    }
+
+    set<pair<int, int>> st;
+    rep(i, L) st.insert({aa[c[i] - 1], bb[d[i] - 1]});
+
+    reverse(all(a));
+    reverse(all(b));
+    multiset<int> ans;
+    debug(right);
+    rep(i, N) {
+        rep(j, M) {
+            if (a[i] + b[j] < left) break;
+            if (st.count({a[i], b[j]})) continue;
+            ans.insert(a[i] + b[j]);
+        }
+    }
+
+    cout << *ans.rbegin() << endl;
 }
