0f08b0222d8f75e21092a6d36de2f157a438d0ab
haruboting <raiu_hk@icloud.com>
2024-01-19 03:02:58 +0900

    solve

--- ABC026/d/main.cpp
+++ ABC026/d/main.cpp
@@ -8,4 +8,17 @@
 using namespace std;
 
 signed main() {
+    int A, B, C;
+    cin >> A >> B >> C;
+
+    double l = 0, r = 300;
+    while (r - l > 1e-12) {
+        double mid = (l + r) / 2;
+        if (A * mid + B * sin(C * mid * M_PI) < 100) {
+            l = mid;
+        } else {
+            r = mid;
+        }
+    }
+    cout << fixed << setprecision(15) << r << endl;
 }
