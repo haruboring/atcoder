7e2ded7e8c450d37d3c9ae3fc6f5134e517bd2cb
haruboting <raiu_hk@icloud.com>
2024-01-18 18:39:44 +0900

    solve

--- CODE_FESTIVAL_2016_qual_C/A.cpp
+++ CODE_FESTIVAL_2016_qual_C/A.cpp
@@ -8,4 +8,34 @@
 using namespace std;
 
 signed main() {
+    int K, T;
+    cin >> K >> T;
+    vector<int> a(T);
+    rep(i, T) cin >> a[i];
+
+    vector<pair<int, int>> v;
+    rep(i, T) v.push_back({a[i], i});
+
+    int ans = 0;
+
+    int pre_cake = -1;
+    rep(_, K) {
+        sort(all(v));
+        reverse(all(v));
+
+        rep(i, T + 1) {
+            if (v[i].first <= 0 || i == T) {
+                v[0].first--;
+                pre_cake = v[0].second;
+                ans++;
+                break;
+            }
+            if (v[i].second == pre_cake) continue;
+            v[i].first--;
+            pre_cake = v[i].second;
+            break;
+        }
+    }
+
+    cout << ans << endl;
 }
