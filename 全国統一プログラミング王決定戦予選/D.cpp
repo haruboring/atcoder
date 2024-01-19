475cd967458122c281098575535967615b4bb685
haruboting <raiu_hk@icloud.com>
2024-01-19 21:23:20 +0900

    solve

--- 全国統一プログラミング王決定戦予選/D.cpp
+++ 全国統一プログラミング王決定戦予選/D.cpp
@@ -8,4 +8,58 @@
 using namespace std;
 
 signed main() {
+    int N, M;
+    cin >> N >> M;
+    vector<int> A(N - 1 + M), B(N - 1 + M);
+    rep(i, N - 1 + M) cin >> A[i] >> B[i];
+
+    vector<vector<int>> G(N), backG(N);
+    vector<int> deg(N);
+    rep(i, N - 1 + M) {
+        A[i]--, B[i]--;
+        G[A[i]].push_back(B[i]);
+        backG[B[i]].push_back(A[i]);
+        deg[B[i]]++;
+    }
+
+    queue<int> q;
+    int root = -1;
+    rep(i, N) {
+        if (deg[i] == 0) {
+            q.push(i);
+            root = i;
+        }
+    }
+    vector<int> order(N);
+    int cnt = 0;
+    while (!q.empty()) {
+        int v = q.front();
+        q.pop();
+        order[v] = cnt++;
+        for (auto nv : G[v]) {
+            deg[nv]--;
+            if (deg[nv] == 0) q.push(nv);
+        }
+    }
+
+    vector<int> parents(N);
+    rep(i, N) {
+        int bv = root;
+        for (auto v : backG[i]) {
+            if (order[v] > order[bv]) {
+                bv = v;
+            }
+        }
+        if (bv != -1) {
+            parents[i] = bv;
+        }
+    }
+
+    rep(i, N) {
+        if (i == root) {
+            cout << 0 << endl;
+            continue;
+        }
+        cout << parents[i] + 1 << endl;
+    }
 }
