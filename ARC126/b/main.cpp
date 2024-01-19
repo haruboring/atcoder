ee1634730a084a0f9347b63371de911ae0495585
haruboting <raiu_hk@icloud.com>
2024-01-20 02:55:07 +0900

    add: ABC185 folder

--- ARC126/b/main.cpp
+++ ARC126/b/main.cpp
@@ -8,4 +8,28 @@
 using namespace std;
 
 signed main() {
+    int N, M;
+    cin >> N >> M;
+    vector<int> a(M), b(M);
+    rep(i, M) cin >> a[i] >> b[i];
+
+    vector<pair<int, int>> vp(M);
+    rep(i, M) vp[i] = make_pair(a[i], -b[i]);
+
+    sort(all(vp));
+    rep(i, M) vp[i].second *= -1;
+
+    vector<int> dp(0);
+
+    for (auto p : vp) {
+        int b = p.second;
+        auto it = lower_bound(all(dp), b);
+        if (it == dp.end()) {
+            dp.push_back(b);
+        } else {
+            *it = b;
+        }
+    }
+
+    cout << dp.size() << endl;
 }
