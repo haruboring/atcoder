51628c5a492dbecbaaa1fd70b8b1a80bafe83d66
haruboting <raiu_hk@icloud.com>
2024-01-19 22:00:28 +0900

    solve

--- ABC219/d/main.cpp
+++ ABC219/d/main.cpp
@@ -8,4 +8,23 @@
 using namespace std;
 
 signed main() {
+    int N;
+    cin >> N;
+    int X, Y;
+    cin >> X >> Y;
+    vector<int> A(N), B(N);
+    rep(i, N) cin >> A[i] >> B[i];
+
+    vector<vector<int>> dp(X + 1, vector<int>(Y + 1, 1e9));
+    dp[0][0] = 0;
+    rep(i, N) {
+        for (int j = X; j >= 0; j--) {
+            for (int k = Y; k >= 0; k--) {
+                if (dp[j][k] == 1e9) continue;
+                dp[min(j + A[i], X)][min(k + B[i], Y)] = min(dp[min(j + A[i], X)][min(k + B[i], Y)], dp[j][k] + 1);
+            }
+        }
+    }
+    if (dp[X][Y] == 1e9) dp[X][Y] = -1;
+    cout << dp[X][Y] << endl;
 }
