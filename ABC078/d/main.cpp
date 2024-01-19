a83e6042b67344708facc8afc8779efda9c5c585
haruboting <raiu_hk@icloud.com>
2024-01-20 05:14:08 +0900

    solve

--- ABC078/d/main.cpp
+++ ABC078/d/main.cpp
@@ -7,5 +7,43 @@
 #define debug(x) cerr << #x << ": " << x << endl
 using namespace std;
 
+// vector<vector<int>> dp(2010, vector<int>(2, -1));
+// int N;
+// vector<int> a(2010);
+// int f(int id, int turn, int x, int y) {
+//     if (id == N) return abs(x - y);
+
+//     if (dp[id][turn] != -1) return dp[id][turn];
+
+//     if (turn == 0) {
+//         int res = -1e9;
+//         repp(i, id, N) {
+//             res = max(res, f(i + 1, 1, a[i], y));
+//         }
+//         return dp[id][turn] = res;
+//     } else {
+//         int res = 1e9;
+//         repp(i, id, N) {
+//             res = min(res, f(i + 1, 0, x, a[i]));
+//         }
+//         return dp[id][turn] = res;
+//     }
+// }
+
+// signed main() {
+//     int Z, W;
+//     cin >> N >> Z >> W;
+//     rep(i, N) cin >> a[i];
+
+//     cout << f(0, 0, Z, W) << endl;
+// }
+
 signed main() {
+    int N, Z, W;
+    cin >> N >> Z >> W;
+    vector<int> a(N);
+    rep(i, N) cin >> a[i];
+
+    int ans = max({abs(a[N - 1] - W), abs(a[N - 1] - a[N - 2])});
+    cout << ans << endl;
 }
