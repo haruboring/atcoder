e744ac4b04702f1fda2fc64cef71df7d830dcbad
haruboting <raiu_hk@icloud.com>
2024-01-20 04:29:15 +0900

    solve

--- ABC185/e/main.cpp
+++ ABC185/e/main.cpp
@@ -8,4 +8,19 @@
 using namespace std;
 
 signed main() {
+    int N, M;
+    cin >> N >> M;
+    vector<int> A(N), B(M);
+    rep(i, N) cin >> A[i];
+    rep(i, M) cin >> B[i];
+
+    vector<vector<int>> dp(N + 1, vector<int>(M + 1, 1e9));
+    rep(i, N + 1) dp[i][0] = i;
+    rep(j, M + 1) dp[0][j] = j;
+    rep(i, N) {
+        rep(j, M) {
+            dp[i + 1][j + 1] = min({dp[i][j + 1] + 1, dp[i + 1][j] + 1, dp[i][j] + (A[i] != B[j])});
+        }
+    }
+    cout << dp[N][M] << endl;
 }
