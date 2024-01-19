54970313051e7156ae282f5e8f0664c5b356ed42
haruboting <raiu_hk@icloud.com>
2024-01-19 22:15:38 +0900

    solve

--- ABC090/d/main.cpp
+++ ABC090/d/main.cpp
@@ -8,4 +8,14 @@
 using namespace std;
 
 signed main() {
+    int N, K;
+    cin >> N >> K;
+
+    int ans = 0;
+
+    repp(i, 1, N + 1) {
+        ans += (max(0LL, i - K) * (N / i)) + max(0LL, N % i - K + 1);
+    }
+    if (K == 0) ans -= N;
+    cout << ans << endl;
 }
