32ab64784031bdda1c40a54b4373410d56e9d14e
haruboting <raiu_hk@icloud.com>
2024-01-21 07:40:30 +0900

    solve

--- ABC161/f/main.cpp
+++ ABC161/f/main.cpp
@@ -8,4 +8,34 @@
 using namespace std;
 
 signed main() {
+    int N;
+    cin >> N;
+
+    set<int> st;
+    for (int j = 1; j * j <= (N - 1); j++) {
+        if ((N - 1) % j != 0) continue;
+        st.insert(j);
+        st.insert((N - 1) / j);
+    }
+    int cN = N;
+    for (int i = 2; i * i <= cN; i++) {
+        int cnt = 0;
+        while (N % i == 0) {
+            N /= i;
+            cnt++;
+        }
+
+        rep(j, cnt) N *= i;
+
+        int tmp = 1;
+        rep(j, cnt) {
+            tmp *= i;
+            if ((cN / tmp - 1) % i == 0) st.insert(i);
+        }
+    }
+
+    st.insert(cN);
+    if (st.count(1)) st.erase(1);
+
+    cout << st.size() << endl;
 }
