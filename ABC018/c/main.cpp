cb9501cd3ee2ae030a31385a7375b7c904f8a834
haruboting <raiu_hk@icloud.com>
2024-01-24 16:26:27 +0900

    solve

--- ABC018/c/main.cpp
+++ ABC018/c/main.cpp
@@ -8,4 +8,31 @@
 using namespace std;
 
 signed main() {
+    int R, C, K;
+    cin >> R >> C >> K;
+    vector<string> S(R);
+    rep(i, R) cin >> S[i];
+
+    int cnt = 0;
+    repp(i, K - 1, R - K + 2) {
+        repp(j, K - 1, C - K + 2) {
+            bool ok = true;
+            rep(k, K) {
+                rep(l, k + 1) {
+                    int m = k - l;
+                    if (!(i + l < R && j - m >= 0 && i - l >= 0 && j + m < C)) {
+                        ok = false;
+                        continue;
+                    }
+                    if (S[i + l][j - m] == 'x') ok = false;
+                    if (S[i + l][j + m] == 'x') ok = false;
+                    if (S[i - l][j - m] == 'x') ok = false;
+                    if (S[i - l][j + m] == 'x') ok = false;
+                }
+            }
+            if (ok) cnt++;
+        }
+    }
+
+    cout << cnt << endl;
 }
