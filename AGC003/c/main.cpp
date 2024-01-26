87f6d08cd3cf0320daefe034f37a3bd180519d67
haruboting <raiu_hk@icloud.com>
2024-01-26 09:04:44 +0900

    solve

--- AGC003/c/main.cpp
+++ AGC003/c/main.cpp
@@ -8,4 +8,32 @@
 using namespace std;
 
 signed main() {
+    int N;
+    cin >> N;
+    vector<int> A(N);
+    rep(i, N) cin >> A[i];
+
+    vector<int> zA(0), oA(0);
+    rep(i, N) {
+        if (i % 2 == 0) {
+            zA.push_back(A[i]);
+        } else {
+            oA.push_back(A[i]);
+        }
+    }
+    sort(all(zA));
+    sort(all(oA));
+
+    int ans = 0;
+    sort(all(A));
+    set<int> even;
+    rep(i, N) {
+        if (i % 2 == 0) {
+            even.insert(A[i]);
+        }
+    }
+    rep(i, zA.size()) {
+        if (!even.count(zA[i])) ans++;
+    }
+    cout << ans << endl;
 }
