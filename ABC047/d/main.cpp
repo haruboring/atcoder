1444b31664115608e6f73134437e81e104b5e0a7
haruboting <raiu_hk@icloud.com>
2024-01-29 03:51:54 +0900

    solve

--- ABC047/d/main.cpp
+++ ABC047/d/main.cpp
@@ -8,4 +8,30 @@
 using namespace std;
 
 signed main() {
+    int N, T;
+    cin >> N >> T;
+    vector<int> A(N);
+    rep(i, N) cin >> A[i];
+
+    int st = A[0];
+    map<int, int> mp;
+    rep(i, N - 1) {
+        if (A[i + 1] < A[i]) {
+            mp[A[i] - st]++;
+            if (A[i + 1] < st) st = A[i + 1];
+            debug(st);
+        }
+        if (i == N - 2 && A[i + 1] >= A[i]) {
+            mp[A[i + 1] - st]++;
+        }
+    }
+
+    int max_diff = -1;
+    for (auto p : mp) {
+        max_diff = max(max_diff, p.first);
+    }
+
+    debug(max_diff);
+
+    cout << mp[max_diff] << endl;
 }
