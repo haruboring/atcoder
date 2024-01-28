cf9ee01bfd8b04a8243046f2827bdcdb851a784b
haruboting <raiu_hk@icloud.com>
2024-01-29 01:47:10 +0900

    solve

--- ABC269/e/main.cpp
+++ ABC269/e/main.cpp
@@ -8,4 +8,47 @@
 using namespace std;
 
 signed main() {
+    int N;
+    cin >> N;
+
+    int ai = 0, aj = 0;
+
+    int il = 0, ir = N - 1, jl = 0, jr = N - 1;
+    while (ir - il > 0) {
+        int im = (il + ir) / 2;
+
+        debug(im);
+        cout << "? " << il + 1 << " " << im + 1 << " " << jl + 1 << " " << jr + 1 << endl;
+        int cnt;
+        cin >> cnt;
+
+        if (cnt < im - il + 1) {
+            ir = im;
+        } else {
+            il = im + 1;
+        }
+
+        debug(il);
+        debug(ir);
+    }
+
+    ai = il;
+
+    il = 0, ir = N - 1, jl = 0, jr = N - 1;
+    while (jr - jl > 0) {
+        int jm = (jl + jr) / 2;
+        cout << "? " << il + 1 << " " << ir + 1 << " " << jl + 1 << " " << jm + 1 << endl;
+        int cnt;
+        cin >> cnt;
+
+        if (cnt < jm - jl + 1) {
+            jr = jm;
+        } else {
+            jl = jm + 1;
+        }
+    }
+
+    aj = jl;
+
+    cout << "! " << ai + 1 << " " << aj + 1 << endl;
 }
