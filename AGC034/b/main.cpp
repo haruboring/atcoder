ac69be1e9340b3fb8b95689f8bc37797d3787587
haruboting <raiu_hk@icloud.com>
2024-01-30 03:28:15 +0900

    solve

--- AGC034/b/main.cpp
+++ AGC034/b/main.cpp
@@ -8,4 +8,22 @@
 using namespace std;
 
 signed main() {
+    string S;
+    cin >> S;
+
+    int N = S.size();
+    int ans = 0;
+    int cnt_A = 0;
+    rep(i, N) {
+        if (S[i] == 'A') {
+            cnt_A++;
+        } else if (i + 1 < N && S[i] == 'B' && S[i + 1] == 'C') {
+            ans += cnt_A;
+            i++;
+        } else {
+            cnt_A = 0;
+        }
+    }
+
+    cout << ans << endl;
 }
