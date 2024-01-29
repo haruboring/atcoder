fad9883610aae70e0afeb3f5641103d2ea3d60f3
haruboting <raiu_hk@icloud.com>
2024-01-29 17:43:34 +0900

    solve

--- 第5回 ドワンゴからの挑戦状 予選/B.cpp
+++ 第5回 ドワンゴからの挑戦状 予選/B.cpp
@@ -1,4 +1,4 @@
-#include "atcoder/all"
+// #include "atcoder/all"
 #include "bits/stdc++.h"
 #define int long long
 #define all(v) v.begin(), v.end()
@@ -8,4 +8,46 @@
 using namespace std;
 
 signed main() {
+    int N, K;
+    cin >> N >> K;
+    vector<int> a(N);
+    rep(i, N) cin >> a[i];
+
+    multiset<int> st;
+    vector<int> sum(N + 1);
+    rep(i, N) {
+        sum[i + 1] = sum[i] + a[i];
+    }
+
+    rep(i, N - 1) {
+        repp(j, i, N) {
+            // debug(sum[j + 1] - sum[i]);
+            st.insert(sum[j + 1] - sum[i]);
+        }
+    }
+
+    int tmp = 1;
+    rep(i, 62) tmp *= 2;
+    int ans = 0;
+    for (int i = 62; i >= 0; i--) {
+        int cnt = 0;
+        for (auto b : st) {
+            bitset<63> bit(b);
+            if (bit.test(i)) cnt++;
+        }
+        if (cnt >= K) {
+            ans += tmp;
+            multiset<int> tmp_st;
+            for (auto b : st) {
+                bitset<63> bit(b);
+                if (bit.test(i)) {
+                    tmp_st.insert(b);
+                }
+            }
+            st = tmp_st;
+        }
+        tmp /= 2;
+    }
+
+    cout << ans << endl;
 }
