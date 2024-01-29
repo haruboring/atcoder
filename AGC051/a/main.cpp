497c440006967ce624d8f1a529885f472051b127
haruboting <raiu_hk@icloud.com>
2024-01-29 20:15:04 +0900

    solve

--- AGC051/a/main.cpp
+++ AGC051/a/main.cpp
@@ -7,5 +7,42 @@
 #define debug(x) cerr << #x << ": " << x << endl
 using namespace std;
 
+const int MAX = 2020000;
+int MOD = 998244353;
+int fac[MAX], finv[MAX], inv[MAX];
+
+void COMinit() {
+    fac[0] = fac[1] = 1;
+    finv[0] = finv[1] = 1;
+    inv[1] = 1;
+    for (int i = 2; i < MAX; i++) {
+        fac[i] = fac[i - 1] * i % MOD;
+        inv[i] = MOD - inv[MOD % i] * (MOD / i) % MOD;
+        finv[i] = finv[i - 1] * inv[i] % MOD;
+    }
+}
+
+int COM(int n, int k) {
+    if (n < k) return 0;
+    if (n < 0 || k < 0) return 0;
+    return fac[n] * (finv[k] * finv[n - k] % MOD) % MOD;
+}
+
+int fast_pow(int a, int b, int MOD) {
+    int ans = 1;
+    while (b > 0) {
+        if (b & 1) ans = ans * a % MOD;
+        a = a * a % MOD;
+        b >>= 1;
+    }
+    return ans;
+}
+
 signed main() {
+    int d;
+    cin >> d;
+
+    COMinit();
+
+    cout << COM(2 * d, d) * fast_pow(2, MOD - 2, MOD) % MOD << endl;
 }
