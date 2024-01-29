d7f790dd6fa1aeb9080ff1fa80444ffa49f8884a
haruboting <raiu_hk@icloud.com>
2024-01-29 16:38:55 +0900

    solve

--- ABC099/c/main.cpp
+++ ABC099/c/main.cpp
@@ -8,4 +8,26 @@
 using namespace std;
 
 signed main() {
+    int N;
+    cin >> N;
+
+    set<int> st;
+    rep(i, 20) {
+        if (pow(6, i) > N) break;
+        st.insert(pow(6, i));
+    }
+    rep(i, 20) {
+        if (pow(9, i) > N) break;
+        st.insert(pow(9, i));
+    }
+
+    vector<int> dp(N + 1, 1e9);
+    dp[0] = 0;
+    rep(i, N) {
+        for (auto x : st) {
+            if (i + x > N) continue;
+            dp[i + x] = min(dp[i + x], dp[i] + 1);
+        }
+    }
+    cout << dp[N] << endl;
 }
