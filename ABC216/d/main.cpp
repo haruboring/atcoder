22000157610be95ed15506736f764c0024cd98f8
haruboting <raiu_hk@icloud.com>
2024-01-30 19:51:33 +0900

    solve

--- ABC216/d/main.cpp
+++ ABC216/d/main.cpp
@@ -8,4 +8,49 @@
 using namespace std;
 
 signed main() {
+    int N, M;
+    cin >> N >> M;
+    vector<queue<int>> tutu(M);
+    rep(i, M) {
+        int k;
+        cin >> k;
+        rep(j, k) {
+            int a;
+            cin >> a;
+            a--;
+            tutu[i].push(a);
+        }
+    }
+
+    queue<int> q;
+
+    map<int, vector<int>> mp;
+    rep(i, M) {
+        int a = tutu[i].front();
+        mp[a].push_back(i);
+        if (mp[a].size() == 2) q.push(a);
+    }
+
+    while (!q.empty()) {
+        int a = q.front();
+        q.pop();
+        for (auto i : mp[a]) {
+            tutu[i].pop();
+            if (!tutu[i].empty()) {
+                int b = tutu[i].front();
+                mp[b].push_back(i);
+                if (mp[b].size() == 2) q.push(b);
+            }
+        }
+    }
+
+    bool ok = true;
+    rep(i, M) {
+        if (!tutu[i].empty()) ok = false;
+    }
+
+    if (ok)
+        cout << "Yes" << endl;
+    else
+        cout << "No" << endl;
 }
