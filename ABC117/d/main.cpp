586736a2aded425bfc3ba206e57d6a37a8800a36
haruboting <raiu_hk@icloud.com>
2024-01-30 20:25:39 +0900

    solve

--- ABC117/d/main.cpp
+++ ABC117/d/main.cpp
@@ -8,4 +8,39 @@
 using namespace std;
 
 signed main() {
+    int N, K;
+    cin >> N >> K;
+    vector<int> A(N);
+    rep(i, N) cin >> A[i];
+
+    vector<int> bit_cnt(50, 0);
+    rep(i, N) {
+        rep(j, 50) {
+            if (A[i] & (1LL << j)) bit_cnt[j]++;
+        }
+    }
+
+    int tmp = 1;
+    rep(i, 50) {
+        tmp *= 2;
+    }
+    int X = 0;
+    rep(i, 50) {
+        tmp /= 2;
+        // 7 < 33
+        if (X + tmp <= K && bit_cnt[49 - i] < N - bit_cnt[49 - i]) {
+            X += tmp;
+            debug(49 - i);
+        }
+    }
+    debug(X);
+
+    int ans = 0;
+    rep(i, N) {
+        ans += X ^ A[i];
+        int a = X ^ A[i];
+        debug(a);
+    }
+
+    cout << ans << endl;
 }
