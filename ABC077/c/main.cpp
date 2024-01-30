aaa0dc5002130cb2317c2b3ca560668dcfbfb257
haruboting <raiu_hk@icloud.com>
2024-01-30 20:43:15 +0900

    solve

--- ABC077/c/main.cpp
+++ ABC077/c/main.cpp
@@ -8,4 +8,25 @@
 using namespace std;
 
 signed main() {
+    int N;
+    cin >> N;
+    vector<int> A(N), B(N), C(N);
+    rep(i, N) cin >> A[i];
+    rep(i, N) cin >> B[i];
+    rep(i, N) cin >> C[i];
+
+    sort(all(A));
+    sort(all(B));
+    sort(all(C));
+
+    vector<int> b_cnt = vector<int>(N, 0);
+    rep(i, N){
+        b_cnt[i] = lower_bound(all(A), B[i]) - A.begin();
+    }
+    int ans = 0;
+    rep(i, N){
+        ans += b_cnt[i] * (N - (upper_bound(all(C), B[i]) - C.begin()));
+    }
+
+    cout << ans << endl;
 }
