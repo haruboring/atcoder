67fb32a3fb93dd6339d81fc275da4f7759debd51
haruboting <raiu_hk@icloud.com>
2024-01-31 13:17:22 +0900

    solve

--- ABC006/c/main.cpp
+++ ABC006/c/main.cpp
@@ -8,4 +8,25 @@
 using namespace std;
 
 signed main() {
+    int N, M;
+    cin >> N >> M;
+
+    if (M < 2 * N || 4 * N < M) {
+        cout << "-1 -1 -1" << endl;
+        return 0;
+    }
+
+    int legs = 3 * N;
+    if (legs == M) {
+        cout << 0 << " " << N << " " << 0 << endl;
+        return 0;
+    }
+    if (legs < M) {
+        int diff = M - legs;
+        cout << 0 << " " << N - diff << " " << diff << endl;
+        return 0;
+    }
+    int diff = legs - M;
+    cout << diff << " " << N - diff << " " << 0 << endl;
+    return 0;
 }
