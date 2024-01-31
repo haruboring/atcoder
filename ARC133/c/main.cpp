85fa1df6c945b332777bc55db6b13b8f3756fb77
haruboting <raiu_hk@icloud.com>
2024-01-31 15:34:10 +0900

    solve

--- ARC133/c/main.cpp
+++ ARC133/c/main.cpp
@@ -8,4 +8,26 @@
 using namespace std;
 
 signed main() {
+    int H, W, K;
+    cin >> H >> W >> K;
+    vector<int> A(H), B(W);
+    rep(i, H) cin >> A[i];
+    rep(i, W) cin >> B[i];
+
+    int a_sum = accumulate(all(A), 0LL), b_sum = accumulate(all(B), 0LL);
+    if (a_sum % K != b_sum % K) {
+        cout << -1 << endl;
+        return 0;
+    }
+
+    int tmp1 = 0;
+    rep(i, W) {
+        tmp1 += (((K - 1) * H) - B[i] + K) % K;
+    }
+    int tmp2 = 0;
+    rep(i, H) {
+        tmp2 += (((K - 1) * W) - A[i] + K) % K;
+    }
+
+    cout << (K - 1) * H * W - max(tmp1, tmp2) << endl;
 }
