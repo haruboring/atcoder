4eac172067268683e8f23f06ffd0b56381b27349
haruboting <raiu_hk@icloud.com>
2024-01-31 11:58:19 +0900

    solve

--- ARC005/b/main.cpp
+++ ARC005/b/main.cpp
@@ -8,4 +8,32 @@
 using namespace std;
 
 signed main() {
+    int N;
+    cin >> N;
+    vector<int> d(N);
+    rep(i, N) cin >> d[i];
+
+    int longest = accumulate(all(d), 0LL);
+    cout << longest << endl;
+
+    vector<int> sum(N + 1, 0);
+    rep(i, N) sum[i + 1] = sum[i] + d[i];
+
+    if (N == 1) {
+        cout << longest << endl;
+        return 0;
+    } else if (N == 2) {
+        cout << abs(d[0] - d[1]) << endl;
+    } else {
+        int shortest = 1e9;
+        rep(i, N - 2) {
+            repp(j, i + 1, N - 1) {
+                int a = sum[i + 1];
+                int b = sum[j + 1] - sum[i + 1];
+                int c = sum[N] - sum[j + 1];
+                shortest = min(shortest, max(0LL, max({a, b, c}) - (a + b + c - max({a, b, c}))));
+            }
+        }
+        cout << shortest << endl;
+    }
 }
