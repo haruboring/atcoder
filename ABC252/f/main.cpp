8cc347f77336fe96abb146e5dc41ddcbdfe77e53
haruboting <raiu_hk@icloud.com>
2024-02-02 06:20:01 +0900

    solve

--- ABC252/f/main.cpp
+++ ABC252/f/main.cpp
@@ -8,4 +8,26 @@
 using namespace std;
 
 signed main() {
+    int N, L;
+    cin >> N >> L;
+    vector<int> A(N);
+    rep(i, N) cin >> A[i];
+
+    int sum = accumulate(all(A), 0LL);
+
+    priority_queue<int, vector<int>, greater<int>> pq;
+
+    rep(i, N) pq.push(A[i]);
+    if (L - sum > 0) pq.push(L - sum);
+
+    int ans = 0;
+    while (pq.size() > 1) {
+        int a = pq.top();
+        pq.pop();
+        int b = pq.top();
+        pq.pop();
+        ans += a + b;
+        pq.push(a + b);
+    }
+    cout << ans << endl;
 }
