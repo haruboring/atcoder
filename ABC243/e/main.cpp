cbf2d48329656a0d18008dc713eae69a581fbd24
haruboting <raiu_hk@icloud.com>
2024-02-03 16:08:12 +0900

    solve

--- ABC243/e/main.cpp
+++ ABC243/e/main.cpp
@@ -8,4 +8,43 @@
 using namespace std;
 
 signed main() {
+    int N, M;
+    cin >> N >> M;
+    vector<int> A(M), B(M), C(M);
+    rep(i, M) cin >> A[i] >> B[i] >> C[i];
+
+    vector<vector<int>> dist(N, vector<int>(N, 1e18));
+    rep(i, M) {
+        A[i]--;
+        B[i]--;
+        dist[A[i]][B[i]] = C[i];
+        dist[B[i]][A[i]] = C[i];
+    }
+    rep(i, N) {
+        dist[i][i] = 0;
+    }
+
+    rep(k, N) {
+        rep(i, N) {
+            rep(j, N) {
+                dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
+            }
+        }
+    }
+
+    int ans = 0;
+    rep(i, M) {
+        bool ok = false;
+        rep(j, N) {
+            if (A[i] == j || j == B[i]) continue;
+            if (dist[A[i]][j] + dist[j][B[i]] <= C[i]) {
+                ok = true;
+                break;
+            }
+        }
+        if (ok) {
+            ans++;
+        }
+    }
+    cout << ans << endl;
 }
