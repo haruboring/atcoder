fcd7ed5bcdfe1305c5164cc06d2a39b3a2e1a9a9
haruboting <raiu_hk@icloud.com>
2024-02-09 04:07:09 +0900

    solve

--- ABC260/e/main.cpp
+++ ABC260/e/main.cpp
@@ -8,4 +8,49 @@
 using namespace std;
 
 signed main() {
+    int N, M;
+    cin >> N >> M;
+    vector<int> A(N), B(N);
+    rep(i, N) cin >> A[i] >> B[i];
+
+    rep(i, N) {
+        A[i]--;
+        B[i]--;
+    }
+
+    vector<vector<int>> v(M);
+    rep(i, N) {
+        v[A[i]].push_back(i);
+        v[B[i]].push_back(i);
+    }
+
+    vector<int> ans(M);
+    multiset<int> s;
+    int cnt = 0;
+    for (int i = 0, j = 0; i < M;) {
+        while (j < M) {
+            if (cnt == N) break;
+            for (auto x : v[j]) {
+                if (!s.count(x)) cnt++;
+                s.insert(x);
+            }
+            j++;
+        }
+        if (cnt == N) {
+            ans[j - i - 1]++;
+            if (i != 0) ans[M - i]--;
+        }
+
+        for (auto x : v[i]) {
+            s.erase(s.find(x));
+            if (!s.count(x)) cnt--;
+        }
+        i++;
+    }
+
+    rep(i, M) {
+        if (i != 0) ans[i] += ans[i - 1];
+        cout << ans[i] << " ";
+    }
+    cout << endl;
 }
