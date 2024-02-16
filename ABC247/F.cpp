5201830b62294fb8a4b8c23430118bc1f6e0c4b3
haruboting <raiu_hk@icloud.com>
2024-02-16 23:51:31 +0900

    solve

--- ABC247/F.cpp
+++ ABC247/F.cpp
@@ -7,6 +7,28 @@
 #define debug(x) cerr << #x << ": " << x << endl
 using namespace std;
 
+vector<int> par(200000 + 100);
+
+void init(int n) {
+    rep(i, n) par[i] = i;
+}
+
+int root(int x) {
+    if (par[x] == x) return x;
+    return par[x] = root(par[x]);
+}
+
+int unite(int x, int y) {
+    int rx = root(x);
+    int ry = root(y);
+    if (ry < rx) swap(rx, ry);
+    if (rx == ry) {
+        return 0;
+    } else {
+        return par[rx] = ry;
+    }
+}
+
 signed main() {
     int N;
     cin >> N;
@@ -14,5 +36,32 @@ signed main() {
     rep(i, N) cin >> P[i];
     rep(i, N) cin >> Q[i];
 
-    
+    init(N);
+    rep(i, N) {
+        P[i]--;
+        Q[i]--;
+        unite(P[i], Q[i]);
+    }
+
+    map<int, int> mp;
+    rep(i, N) {
+        mp[root(i)]++;
+    }
+
+    vector<int> dp(N + 10);
+    dp[1] = 1;
+    dp[2] = 3;
+    dp[3] = 4;
+    repp(i, 4, N + 1) {
+        dp[i] = dp[i - 1] + dp[i - 2];
+        dp[i] %= 998244353;
+    }
+
+    int ans = 1;
+    for (auto [k, v] : mp) {
+        ans *= dp[v];
+        ans %= 998244353;
+    }
+
+    cout << ans << endl;
 }
