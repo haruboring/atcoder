a0c955a89722946e374d0238cc1a159321cbb354
haruboting <raiu_hk@icloud.com>
2024-02-18 09:32:46 +0900

    solve

--- ABC330/f/main.cpp
+++ ABC330/f/main.cpp
@@ -8,4 +8,48 @@
 using namespace std;
 
 signed main() {
+    int N, K;
+    cin >> N >> K;
+    vector<int> X(N), Y(N);
+    rep(i, N) cin >> X[i] >> Y[i];
+    sort(all(X));
+    sort(all(Y));
+
+    int l = -1, r = 1e9 + 100;
+    while (r - l > 1) {
+        int m = (l + r) / 2;
+
+        vector<int> addX(N), addY(N);
+        rep(i, N) {
+            addX[i] = X[i] - m;
+            addY[i] = Y[i] - m;
+        }
+
+        vector<int> xx, yy;
+        merge(all(X), all(addX), back_inserter(xx));
+        merge(all(Y), all(addY), back_inserter(yy));
+        int xl = xx[N], yl = yy[N];
+
+        int cost = 0;
+        rep(i, N) {
+            if (xl <= X[i] && X[i] <= xl + m && yl <= Y[i] && Y[i] <= yl + m) continue;
+            if (X[i] < xl) {
+                cost += xl - X[i];
+            } else if (xl + m < X[i]) {
+                cost += X[i] - (xl + m);
+            }
+            if (Y[i] < yl) {
+                cost += yl - Y[i];
+            } else if (yl + m < Y[i]) {
+                cost += Y[i] - (yl + m);
+            }
+        }
+
+        if (cost > K) {
+            l = m;
+        } else {
+            r = m;
+        }
+    }
+    cout << r << endl;
 }
