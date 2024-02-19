e86f642d4dd26eccff58185b408dfff32e30c929
haruboting <raiu_hk@icloud.com>
2024-02-19 16:00:24 +0900

    finish

--- AHC030/test.sh
+++ AHC030/test.sh
@@ -10,7 +10,7 @@ for i in $(seq -f "%04g" 0 99); do
     input_file_name="${i}.txt"
     echo "input_file_name: ${input_file_name}"
     # cargo runを実行し、標準エラー出力から数値を抽出してsumに加算
-    error_output=$(cd tools && cargo run -r --bin tester ./a.out < in/${input_file_name} > out16/${input_file_name})
+    error_output=$(cd tools && cargo run -r --bin tester ./a.out < in/${input_file_name} > out17/${input_file_name})
 done
 
 # input_file_name: 0087.txt
