dad4dc0746bccfcd5997f9bc5f4b1727af2e4d25
haruboting <raiu_hk@icloud.com>
2024-04-02 18:47:14 +0900

    solve

--- ABC338/e/main.cpp
+++ ABC338/e/main.cpp
@@ -7,6 +7,12 @@
 #define debug(x) cerr << #x << ": " << x << endl
 using namespace std;
 
+int op(int a, int b) { return max(a, b); }
+int e() { return 1e18; }
+int mapping(int f, int x) { return f + x; }
+int composition(int f, int g) { return f + g; }
+int id() { return 0; }
+
 signed main() {
     int N;
     cin >> N;
@@ -18,52 +24,33 @@ signed main() {
         B[i]--;
     }
 
-    int diff = -1;
+    map<int, int> mp;
     rep(i, N) {
-        if (abs(A[i] - B[i]) == 1) {
-            diff = max(A[i], B[i]);
-            break;
+        mp[A[i]] = B[i];
+        mp[B[i]] = A[i];
+    }
+
+    set<int> decided;
+
+    int scope = 1e9;
+    rep(i, 2 * N) {
+        if (decided.count(i)) continue;
+
+        int a = i, b = mp[i];
+        if (a > scope) scope = 1e9;
+        if ((b - a + 2 * N) % N == 1) {
+            decided.insert(i);
+            decided.insert(mp[i]);
+            continue;
         }
-    }
-    debug(diff);
-    rep(i, N) {
-        A[i] = (A[i] - diff + 2 * N) % (2 * N);
-        B[i] = (B[i] - diff + 2 * N) % (2 * N);
-        // debug(A[i]);
-        // debug(B[i]);
-    }
-
-    rep(i, N) {
-        if (A[i] > B[i]) swap(A[i], B[i]);
-    }
-
-    vector<pair<int, int>> AB;
-    rep(i, N) AB.push_back(make_pair(A[i], B[i]));
-    sort(all(AB));
-
-    bool sp = true;
-    swap(AB[0].first, AB[0].second);
-    rep(i, N) {
-        // cout << AB[i].first << " " << AB[i].second << endl;
-        if (AB[i].first % 2 != 1) sp = false;
-        if (AB[i].second % 2 != 0) sp = false;
-    }
-    swap(AB[0].first, AB[0].second);
-
-    if (sp) {
-        cout << "No" << endl;
-        return 0;
-    }
-
-    rep(i, N) {
-        if (AB[i].first != i) {
+        if (b > scope) {
             cout << "Yes" << endl;
             return 0;
         }
-        if (AB[i].second != 2 * N - i - 1) {
-            cout << "Yes" << endl;
-            return 0;
-        }
+        scope = b;
+
+        decided.insert(i);
+        decided.insert(mp[i]);
     }
 
     cout << "No" << endl;
