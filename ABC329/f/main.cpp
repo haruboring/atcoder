e19e8b4a20f6fe2a3f87afa1d3e03c23e1259c4e
haruboting <raiu_hk@icloud.com>
2024-04-04 17:16:14 +0900

    solve

--- ABC329/f/main.cpp
+++ ABC329/f/main.cpp
@@ -8,4 +8,32 @@
 using namespace std;
 
 signed main() {
+    int N, Q;
+    cin >> N >> Q;
+    vector<int> C(N);
+    rep(i, N) cin >> C[i];
+
+    vector<set<int>> sv(N);
+    rep(i, N) {
+        sv[i].insert(C[i]);
+    }
+    map<int, int> mp;
+    rep(i, N) mp[i] = i;
+    rep(_, Q) {
+        int a, b;
+        cin >> a >> b;
+        a--, b--;
+
+        int a_size = sv[mp[a]].size();
+        int b_size = sv[mp[b]].size();
+        if (a_size < b_size) {
+            sv[mp[b]].insert(all(sv[mp[a]]));
+            sv[mp[a]].clear();
+        } else {
+            sv[mp[a]].insert(all(sv[mp[b]]));
+            sv[mp[b]].clear();
+            swap(mp[a], mp[b]);
+        }
+        cout << sv[mp[b]].size() << endl;
+    }
 }
