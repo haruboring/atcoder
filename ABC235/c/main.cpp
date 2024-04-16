40d7738577dda585a197cb8e98c4a0765cd6715a
haruboting <raiu_hk@icloud.com>
2024-04-16 10:44:22 +0900

    solve

--- ABC235/c/main.cpp
+++ ABC235/c/main.cpp
@@ -8,4 +8,25 @@
 using namespace std;
 
 signed main() {
+    int N, Q;
+    cin >> N >> Q;
+    vector<int> A(N);
+    rep(i, N) cin >> A[i];
+
+    map<pair<int, int>, int> mp;
+    map<int, int> cnt;
+    rep(i, N) {
+        cnt[A[i]]++;
+        mp[{A[i], cnt[A[i]]}] = i;
+    }
+
+    rep(_, Q) {
+        int x, k;
+        cin >> x >> k;
+        if (mp.count({x, k}) == 0) {
+            cout << -1 << endl;
+        } else {
+            cout << mp[{x, k}] + 1 << endl;
+        }
+    }
 }
