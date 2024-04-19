5244619d22dc3f999bdd53c82ec4a3c9673f1fe0
haruboting <raiu_hk@icloud.com>
2024-04-19 18:40:57 +0900

    s

--- ABC331/f/main.cpp
+++ ABC331/f/main.cpp
@@ -7,5 +7,72 @@
 #define debug(x) cerr << #x << ": " << x << endl
 using namespace std;
 
+using mint = atcoder::modint998244353;
+using S = pair<mint, int>;
+
+vector<mint> P1(1e6 + 100), P2(1e6 + 100), P3(1e6 + 100);
+S op1(S a, S b) {
+    return {a.first * P1[b.second] + b.first, a.second + b.second};
+}
+S op2(S a, S b) {
+    return {a.first + b.first * P2[a.second], a.second + b.second};
+}
+S op3(S a, S b) {
+    return {a.first * P3[b.second] + b.first, a.second + b.second};
+}
+S e() {
+    return S(0, 0);
+}
+
 signed main() {
+    int N, Q;
+    cin >> N >> Q;
+    string s;
+    cin >> s;
+
+    P1[0] = 1;
+    rep(i, N) P1[i + 1] = P1[i] * 2194673;
+    P2[0] = 1;
+    rep(i, N) P2[i + 1] = P2[i] * 1234123;
+    P3[0] = 1;
+    rep(i, N) P3[i + 1] = P3[i] * 2141434;
+
+    vector<S> A(N);
+    rep(i, N) A[i] = S(s[i] - 'a', 1);
+    atcoder::segtree<S, op1, e> seg1(A);
+    atcoder::segtree<S, op2, e> seg2(A);
+    atcoder::segtree<S, op3, e> seg3(A);
+    reverse(all(A));
+    atcoder::segtree<S, op1, e> rseg1(A);
+    atcoder::segtree<S, op2, e> rseg2(A);
+    atcoder::segtree<S, op3, e> rseg3(A);
+
+    rep(_, Q) {
+        int f;
+        cin >> f;
+        if (f == 1) {
+            int x;
+            char c;
+            cin >> x >> c;
+            seg1.set(x - 1, S(c - 'a', 1));
+            seg2.set(x - 1, S(c - 'a', 1));
+            seg3.set(x - 1, S(c - 'a', 1));
+            rseg1.set(N - x, S(c - 'a', 1));
+            rseg2.set(N - x, S(c - 'a', 1));
+            rseg3.set(N - x, S(c - 'a', 1));
+        } else {
+            int l, r;
+            cin >> l >> r;
+            int cnt = 0;
+            if (seg1.prod(l - 1, r).first == rseg1.prod(N - r, N - l + 1).first) cnt++;
+            if (seg2.prod(l - 1, r).first == rseg2.prod(N - r, N - l + 1).first) cnt++;
+            if (seg3.prod(l - 1, r).first == rseg3.prod(N - r, N - l + 1).first) cnt++;
+
+            if (cnt >= 2) {
+                cout << "Yes" << endl;
+            } else {
+                cout << "No" << endl;
+            }
+        }
+    }
 }
--- abc001/d/main.cpp
+++ abc001/d/main.cpp
@@ -8,4 +8,46 @@
 using namespace std;
 
 signed main() {
+    int N;
+    cin >> N;
+    vector<string> S(N);
+    rep(i, N) cin >> S[i];
+
+    map<int, int> s2e;
+    multiset<int> s;
+    rep(i, N) {
+        string ss = S[i].substr(0, 4);
+        string es = S[i].substr(5, 4);
+
+        int si = stoi(ss);
+        si -= si % 5;
+        if (si % 100 == 60) {
+            si -= 60;
+            si += 100;
+        }
+
+        int ei = stoi(es);
+        ei += 4;
+        ei -= ei % 5;
+        if (ei % 100 == 60) {
+            ei -= 60;
+            ei += 100;
+        }
+
+        s.insert(si);
+        s2e[si] = max(s2e[si], ei);
+    }
+
+    while (s.size() > 0) {
+        int si = *s.begin();
+        int ei = s2e[si];
+        s.erase(s.find(si));
+        while (s.size() > 0) {
+            int another_si = *s.begin();
+            if (another_si > ei) break;
+            s.erase(s.find(another_si));
+            ei = max(ei, s2e[another_si]);
+        }
+        cout << setfill('0') << setw(4) << si << "-" << setfill('0') << setw(4) << ei << endl;
+    }
 }
