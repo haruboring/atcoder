4804124840025265ee4bd0a8f4f645ba550c0c3e
haruboting <raiu_hk@icloud.com>
2024-04-21 08:10:24 +0900

    solve

---
+++ abc350/a/main.cpp
@@ -0,0 +1,21 @@
+#include "atcoder/all"
+#include "bits/stdc++.h"
+#define int long long
+#define all(v) v.begin(), v.end()
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define repp(i, m, n) for (int i = m; i < (int)(n); i++)
+#define debug(x) cerr << #x << ": " << x << endl
+using namespace std;
+
+signed main() {
+    string S;
+    cin >> S;
+
+    string num = S.substr(3, 3);
+    if (stoll(num) > 0 && stoll(num) != 316 && stoll(num) < 350) {
+        cout << "Yes" << endl;
+    }
+    else {
+        cout << "No" << endl;
+    }
+}
