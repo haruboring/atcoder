6cda8532a3162b8789d4462c72fe26e63aca212b
haruboting <raiu_hk@icloud.com>
2024-04-27 20:57:37 +0900

    solve

--- abc350/c/main.cpp
+++ abc350/c/main.cpp
@@ -8,4 +8,25 @@
 using namespace std;
 
 signed main() {
+    int N;
+    cin >> N;
+    vector<int> A(N + 1);
+    rep(i, N) cin >> A[i + 1];
+
+    map<int, int> mp;
+    rep(i, N) mp[A[i + 1]] = i + 1;
+
+    vector<pair<int, int>> ans;
+    repp(i, 1, N + 1) {
+        if (A[i] == i) continue;
+        int j = mp[i];
+        ans.push_back({i, j});
+        swap(A[i], A[j]);
+        swap(mp[i], mp[A[j]]);
+    }
+
+    cout << ans.size() << endl;
+    for (auto p : ans) {
+        cout << p.first << " " << p.second << endl;
+    }
 }
--- abc350/d/main.cpp
+++ abc350/d/main.cpp
@@ -7,5 +7,49 @@
 #define debug(x) cerr << #x << ": " << x << endl
 using namespace std;
 
+vector<int> par(200'000 + 100);
+
+void init(int n) {
+    rep(i, n) par[i] = i;
+}
+
+int root(int x) {
+    if (par[x] == x) return x;
+    return par[x] = root(par[x]);
+}
+
+int unite(int x, int y) {
+    int rx = root(x);
+    int ry = root(y);
+    if (ry < rx) swap(rx, ry);
+    if (rx == ry) {
+        return 0;
+    } else {
+        return par[rx] = ry;
+    }
+}
+
 signed main() {
+    int N, M;
+    cin >> N >> M;
+    vector<int> A(M), B(M);
+    rep(i, M) cin >> A[i] >> B[i];
+
+    rep(i, M) A[i]--, B[i]--;
+
+    init(N);
+    rep(i, M) unite(A[i], B[i]);
+    map<int, int> mp;
+    rep(i, N) mp[root(i)]++;
+    map<int, int> edges;
+    rep(i, M) edges[root(A[i])]++;
+
+    int ans = 0;
+    for (auto [a, cnt] : mp) {
+        int tmp = cnt * (cnt - 1) / 2;
+        tmp -= edges[a];
+        ans += tmp;
+    }
+
+    cout << ans << endl;
 }
