0684d5816a33f51b6e1beba71bb3302b86787021
haruboting <raiu_hk@icloud.com>
2024-04-29 13:50:14 +0900

    solve

--- abc287/d/main.cpp
+++ abc287/d/main.cpp
@@ -11,5 +11,34 @@ signed main() {
     string S, T;
     cin >> S >> T;
 
-    
+    vector<bool> lb(T.size() + 1, false), rb(T.size() + 1, false);
+    lb[0] = true;
+    rep(i, T.size()) {
+        if (S[i] == '?' || T[i] == '?') {
+            lb[i + 1] = true;
+        } else if (S[i] == T[i]) {
+            lb[i + 1] = true;
+        } else {
+            break;
+        }
+    }
+    reverse(all(S)), reverse(all(T));
+    rb[0] = true;
+    rep(i, T.size()) {
+        if (S[i] == '?' || T[i] == '?') {
+            rb[i + 1] = true;
+        } else if (S[i] == T[i]) {
+            rb[i + 1] = true;
+        } else {
+            break;
+        }
+    }
+
+    rep(i, T.size() + 1) {
+        if (lb[i] && rb[T.size() - i]) {
+            cout << "Yes" << endl;
+        } else {
+            cout << "No" << endl;
+        }
+    }
 }
