a2076f4f991a2090bac1fcfc2c2dce28b0219168
haruboting <raiu_hk@icloud.com>
2024-04-29 14:29:48 +0900

    solce

--- abc350/e/main.cpp
+++ abc350/e/main.cpp
@@ -7,5 +7,21 @@
 #define debug(x) cerr << #x << ": " << x << endl
 using namespace std;
 
+int N, A;
+double X, Y;
+map<int, double> mp;
+double f(int x) {
+    if (mp.count(x)) return mp[x];
+    double b = 6 * Y / 5 + f(x / 2) / 5 + f(x / 3) / 5 + f(x / 4) / 5 + f(x / 5) / 5 + f(x / 6) / 5;
+    double a = X + f(x / A);
+    return mp[x] = min(a, b);
+}
+
 signed main() {
+    cin >> N >> A >> X >> Y;
+
+    mp[0] = 0;
+    double ans = f(N);
+
+    cout << fixed << setprecision(19) << ans << endl;
 }
