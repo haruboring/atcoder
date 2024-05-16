cccf2f1af806ae9b086824600b72d3b57936bafb
haruboting <raiu_hk@icloud.com>
2024-05-17 02:38:55 +0900

    add

--- abc315/f/main.cpp
+++ abc315/f/main.cpp
@@ -1,11 +1,11 @@
 #include "atcoder/all"
 #include "bits/stdc++.h"
+#define int long long
 #define all(v) v.begin(), v.end()
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
 #define repp(i, m, n) for (int i = m; i < (int)(n); i++)
 #define debug(x) cerr << #x << ": " << x << endl
 using namespace std;
-using ll = long long;
 
 int fast_pow(int a, int b) {
     int ans = 1;
@@ -17,47 +17,27 @@ int fast_pow(int a, int b) {
     return ans;
 }
 
-int main() {
+signed main() {
     int N;
     cin >> N;
     vector<int> X(N), Y(N);
     rep(i, N) cin >> X[i] >> Y[i];
 
-    int lim = 40;
-    double ans = 1e18;
-    queue<tuple<int, double, int>> q;
-
-    repp(i, 1, N) {
-        double dist = sqrt((X[i] - X[0]) * (X[i] - X[0]) + (Y[i] - Y[0]) * (Y[i] - Y[0]));
-
-        q.push({i, dist, 1});
-    }
-
-    while (!q.empty()) {
-        auto [i, dist, cnt] = q.front();
-        q.pop();
-
-        if (N - cnt >= lim) continue;
-
-        if (i == N - 1) {
-            if (cnt == 0) {
-                ans = min(ans, dist);
-            } else {
-                int sk = N - 1 - cnt;
-                if (sk == 0) {
-                    ans = min(ans, dist);
-                } else {
-                    ans = min(ans, dist + (double)fast_pow(2, sk - 1));
-                }
+    vector<vector<double>> dist(N, vector<double>(40, 1e18));
+    dist[0][0] = 0;
+    rep(i, N) {
+        rep(j, 40) {
+            if (dist[i][j] == 1e18) continue;
+            repp(k, i + 1, N) {
+                if (j + k - (i + 1) >= 40) continue;
+                double d = sqrt((X[i] - X[k]) * (X[i] - X[k]) + (Y[i] - Y[k]) * (Y[i] - Y[k]));
+                dist[k][j + k - (i + 1)] = min(dist[k][j + k - (i + 1)], dist[i][j] + d);
             }
-            continue;
-        }
-
-        repp(j, i + 1, N) {
-            double new_dist = sqrt((X[j] - X[i]) * (X[j] - X[i]) + (Y[j] - Y[i]) * (Y[j] - Y[i]));
-            q.push({j, dist + new_dist, cnt + 1});
         }
     }
+
+    double ans = dist[N - 1][0];
+    repp(i, 1, 40) ans = min(ans, dist[N - 1][i] + fast_pow(2, i - 1));
 
     cout << fixed << setprecision(10) << ans << endl;
 }
