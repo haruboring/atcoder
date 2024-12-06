47176b4ff8e3ef7534a594030874981b09048bc0
haruboting <raiu_hk@icloud.com>
2024-12-07 05:44:42 +0900

    solve

--- abc363/e/main.cpp
+++ abc363/e/main.cpp
@@ -13,72 +13,56 @@ signed main() {
     vector<vector<int>> A(H, vector<int>(W));
     rep(i, H) rep(j, W) cin >> A[i][j];
 
-    priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>, greater<tuple<int, int, int>>> coasts;
-    set<pair<int, int>> is_coasts;
+    map<int, set<pair<int, int>>> coasts;
     rep(i, W) {
-        coasts.push({A[0][i], 0, i});
-        coasts.push({A[H - 1][i], H - 1, i});
-        is_coasts.insert({0, i});
-        is_coasts.insert({H - 1, i});
+        coasts[A[0][i]].insert({0, i});
+        coasts[A[H - 1][i]].insert({H - 1, i});
     }
     rep(i, H) {
-        coasts.push({A[i][W - 1], i, W - 1});
-        coasts.push({A[i][0], i, 0});
-        is_coasts.insert({i, W - 1});
-        is_coasts.insert({i, 0});
+        coasts[A[i][W - 1]].insert({i, W - 1});
+        coasts[A[i][0]].insert({i, 0});
     }
 
+    debug(coasts[1].size());
+
     set<pair<int, int>> disappeared;
-    rep(i, Y) {
-        set<pair<int, int>> is_disappearing;
-        while (!coasts.empty()) {
-            queue<pair<int, int>> disappearing;
-            auto [a, y, x] = coasts.top();
-            if (a > i + 1) break;
-            coasts.pop();
+    repp(i, 1, Y + 1) {
+        queue<pair<int, int>> q;
+        for (auto p : coasts[i]) q.push(p);
 
-            disappearing.push({y, x});
-            is_disappearing.insert({y, x});
+        while (!q.empty()) {
+            auto [y, x] = q.front();
+            q.pop();
 
-            while (!disappearing.empty()) {
-                auto [y, x] = disappearing.front();
-                disappearing.pop();
-                disappeared.insert({y, x});
-                if (y > 0 && !disappeared.count({y - 1, x})) {
-                    if (A[y - 1][x] > i + 1 && !is_coasts.count({y - 1, x})) {
-                        coasts.push({A[y - 1][x], y - 1, x});
-                        is_coasts.insert({y - 1, x});
-                    } else if (A[y - 1][x] <= i + 1 && !is_disappearing.count({y - 1, x})) {
-                        disappearing.push({y - 1, x});
-                        is_disappearing.insert({y - 1, x});
-                    }
+            if (disappeared.count({y, x})) continue;
+            disappeared.insert({y, x});
+
+            if (y > 0) {
+                if (A[y - 1][x] > i) {
+                    coasts[A[y - 1][x]].insert({y - 1, x});
+                } else if (!disappeared.count({y - 1, x})) {
+                    q.push({y - 1, x});
                 }
-                if (y < H - 1 && !disappeared.count({y + 1, x})) {
-                    if (A[y + 1][x] > i + 1 && !is_coasts.count({y + 1, x})) {
-                        coasts.push({A[y + 1][x], y + 1, x});
-                        is_coasts.insert({y + 1, x});
-                    } else if (A[y + 1][x] <= i + 1 && !is_disappearing.count({y + 1, x})) {
-                        disappearing.push({y + 1, x});
-                        is_disappearing.insert({y + 1, x});
-                    }
+            }
+            if (y < H - 1) {
+                if (A[y + 1][x] > i) {
+                    coasts[A[y + 1][x]].insert({y + 1, x});
+                } else if (!disappeared.count({y + 1, x})) {
+                    q.push({y + 1, x});
                 }
-                if (x > 0 && !disappeared.count({y, x - 1})) {
-                    if (A[y][x - 1] > i + 1 && !is_coasts.count({y, x - 1})) {
-                        coasts.push({A[y][x - 1], y, x - 1});
-                        is_coasts.insert({y, x - 1});
-                    } else if (A[y][x - 1] <= i + 1 && !is_disappearing.count({y, x - 1})) {
-                        disappearing.push({y, x - 1});
-                        is_disappearing.insert({y, x - 1});
-                    }
+            }
+            if (x > 0) {
+                if (A[y][x - 1] > i) {
+                    coasts[A[y][x - 1]].insert({y, x - 1});
+                } else if (!disappeared.count({y, x - 1})) {
+                    q.push({y, x - 1});
                 }
-                if (x < W - 1 && !disappeared.count({y, x + 1})) {
-                    if (A[y][x + 1] > i + 1 && !is_coasts.count({y, x + 1})) {
-                        coasts.push({A[y][x + 1], y, x + 1});
-                        is_coasts.insert({y, x + 1});
-                    } else if (A[y][x + 1] <= i + 1 && !is_disappearing.count({y, x + 1})) {
-                        disappearing.push({y, x + 1});
-                        is_disappearing.insert({y, x + 1});
-                    }
+            }
+            if (x < W - 1) {
+                if (A[y][x + 1] > i) {
+                    coasts[A[y][x + 1]].insert({y, x + 1});
+                } else if (!disappeared.count({y, x + 1})) {
+                    q.push({y, x + 1});
                 }
             }
         }
