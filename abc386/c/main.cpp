8a5fb077ffbc0a1307a4e4470105b8c0ac7711f2
haruboting <raiu_hk@icloud.com>
2025-02-12 23:54:25 +0900

    upsolve

--- abc386/c/main.cpp
+++ abc386/c/main.cpp
@@ -8,4 +8,75 @@
 using namespace std;
 
 signed main() {
+    int K;
+    cin >> K;
+    string S, T;
+    cin >> S >> T;
+
+    if (S == T) {
+        cout << "Yes" << endl;
+        return 0;
+    }
+
+    if (S.size() == T.size()) {
+        int cnt = 0;
+        rep(i, S.size()) {
+            if (S[i] != T[i]) cnt++;
+        }
+        if (cnt == 1) {
+            cout << "Yes" << endl;
+        } else {
+            cout << "No" << endl;
+        }
+
+        return 0;
+
+    } else if (S.size() + 1 == T.size()) {
+        int cnt = 0;
+        int j = 0;
+        rep(i, T.size()) {
+            if (S[j] != T[i]) {
+                cnt++;
+                if (cnt > 2) {
+                    cout << "No" << endl;
+                    return 0;
+                }
+            } else {
+                j++;
+            }
+        }
+
+        if (cnt == 1) {
+            cout << "Yes" << endl;
+        } else {
+            cout << "No" << endl;
+        }
+
+        return 0;
+    }
+
+    else if (S.size() == T.size() + 1) {
+        int cnt = 0;
+        int j = 0;
+        rep(i, S.size()) {
+            if (S[i] != T[j]) {
+                cnt++;
+            } else {
+                j++;
+            }
+        }
+
+        if (cnt == 1) {
+            cout << "Yes" << endl;
+        } else {
+            cout << "No" << endl;
+        }
+
+        return 0;
+    }
+
+    else {
+        cout << "No" << endl;
+        return 0;
+    }
 }
