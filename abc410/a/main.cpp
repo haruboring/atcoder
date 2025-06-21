a34fbeec9e1e72a1a36302238ecb74149eca453e
haruboting <raiu_hk@icloud.com>
2025-06-21 18:17:43 +0900

    solve

---
+++ abc410/a/main.cpp
@@ -0,0 +1,22 @@
+#include "atcoder/all"
+#include "bits/stdc++.h"
+#define int long long
+#define all(v) v.begin(), v.end()
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define repp(i, m, n) for (int i = m; i < (int)(n); i++)
+#define debug(x) cerr << #x << ": " << x << endl
+using namespace std;
+
+signed main() {
+    int N;
+    cin >> N;
+    vector<int> A(N);
+    rep(i, N) cin >> A[i];
+    int K;
+    cin >> K;
+
+    int ans = 0;
+    rep(i, N) if (A[i] >= K) ans++;
+
+    cout << ans << endl;
+}
