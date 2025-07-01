60a7e6ac4cc66c46528f3b04d312facdaebfa5c2
haruboting <raiu_hk@icloud.com>
2025-07-02 03:44:18 +0900

    solve

---
+++ abc231/a/main.cpp
@@ -0,0 +1,15 @@
+#include "atcoder/all"
+#include "bits/stdc++.h"
+#define int long long
+#define all(v) v.begin(), v.end()
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define repp(i, m, n) for (int i = m; i < (int)(n); i++)
+#define debug(x) cerr << #x << ": " << x << endl
+using namespace std;
+
+signed main() {
+    double D;
+    cin >> D;
+
+    cout << fixed << setprecision(10) << D / 100 << endl;
+}
