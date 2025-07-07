55f4b313c4aea825917fdd1d1046e44eaf506933
haruboting <raiu_hk@icloud.com>
2025-07-07 22:01:10 +0900

    solve

--- abc237/e/main.cpp
+++ abc237/e/main.cpp
@@ -8,4 +8,42 @@
 using namespace std;
 
 signed main() {
+    int N, M;
+    cin >> N >> M;
+    vector<int> H(N);
+    rep(i, N) cin >> H[i];
+    vector<int> U(M), V(M);
+    rep(i, M) cin >> U[i] >> V[i];
+
+    rep(i, M) U[i]--, V[i]--;
+    vector<vector<pair<int, int>>> G(N, vector<pair<int, int>>(0));
+    rep(i, M) {
+        int x = U[i], y = V[i];
+        G[x].push_back({y, max(0LL, H[y] - H[x])});
+        G[y].push_back({x, max(0LL, H[x] - H[y])});
+    }
+
+    vector<int> f(N, 1e18);
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> q;
+    q.push({0, 0});
+    while (!q.empty()) {
+        auto [d, v] = q.top();
+        q.pop();
+
+        if (d >= f[v]) continue;
+        f[v] = d;
+
+        for (auto [nv, dd] : G[v]) {
+            if (d + dd >= f[nv]) continue;
+            q.push({d + dd, nv});
+        }
+    }
+
+    int ans = -1e18;
+    rep(i, N) {
+        // cout << i << ": " << f[i] << endl;
+        ans = max(ans, H[0] - H[i] - f[i]);
+    }
+
+    cout << ans << endl;
 }
