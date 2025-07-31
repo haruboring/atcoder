51b8a74dd1c6baca1cbb2df739d75da72da16dca
haruboting <raiu_hk@icloud.com>
2025-08-01 01:49:09 +0900

    solve

--- arc155/a/main.cpp
+++ arc155/a/main.cpp
@@ -8,4 +8,32 @@
 using namespace std;
 
 signed main() {
+    debug(400378271514996652 % 12);
+    int T;
+    cin >> T;
+
+    rep(_, T) {
+        int N, K;
+        cin >> N >> K;
+        string S;
+        cin >> S;
+
+        // K <= Nだと、直接見たらいいだけ
+        K = K % (2 * N);
+        string S_ = "";
+        rep(i, K) S_ += "-";
+        rep(i, min(N, K)) S_[i] = S[N - 1 - i];
+        rep(i, min(N, K)) S_[K - 1 - i] = S[i];
+        bool ok = true;
+        string ss_ = S + S_;
+        rep(i, ss_.size()) if (ss_[i] != ss_[ss_.size() - 1 - i]) ok = false;
+        string s_s = S_ + S;
+        rep(i, s_s.size()) if (s_s[i] != s_s[s_s.size() - 1 - i]) ok = false;
+
+        if (ok) {
+            cout << "Yes" << endl;
+        } else {
+            cout << "No" << endl;
+        }
+    }
 }
