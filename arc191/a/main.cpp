c4776cef63e498eb42e601b147ecbdbffb0676cf
haruboting <raiu_hk@icloud.com>
2025-08-01 00:50:00 +0900

    add

--- arc191/a/main.cpp
+++ arc191/a/main.cpp
@@ -8,4 +8,25 @@
 using namespace std;
 
 signed main() {
+    int N, M;
+    cin >> N >> M;
+    string S, T;
+    cin >> S >> T;
+
+    map<int, int> m;
+    rep(i, M) m[T[i] - '0']++;
+    rep(i, N) {
+        for (int j = 9; j > S[i] - '0'; j--) {
+            if (m[j] == 0) continue;
+            m[j]--;
+            S[i] = j + '0';
+            break;
+        }
+    }
+
+    bool ok = false;
+    rep(i, N) if (S[i] == T[M - 1]) ok = true;
+    if (!ok) S[N - 1] = T[M - 1];
+
+    cout << S << endl;
 }
