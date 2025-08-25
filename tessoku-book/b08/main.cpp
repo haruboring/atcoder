afa4624fcb50f6bc9b84e38a66d6728171406059
haruboting <raiu_hk@icloud.com>
2025-08-25 22:03:30 +0900

    solve

--- tessoku-book/b08/main.cpp
+++ tessoku-book/b08/main.cpp
@@ -8,4 +8,19 @@
 using namespace std;
 
 signed main() {
+    int N;
+    cin >> N;
+    vector<int> X(N), Y(N);
+    rep(i, N) cin >> X[i] >> Y[i];
+    int Q;
+    cin >> Q;
+    vector<int> A(Q), B(Q), C(Q), D(Q);
+    rep(i, Q) cin >> A[i] >> B[i] >> C[i] >> D[i];
+
+    vector<vector<int>> V(1500 + 10, vector<int>(1500 + 10));
+    rep(i, N) V[X[i]][Y[i]]++;
+    rep(i, 1505) rep(j, 1505) V[i][j + 1] += V[i][j];
+    rep(i, 1505) rep(j, 1505) V[i + 1][j] += V[i][j];
+
+    rep(i, Q) cout << V[C[i]][D[i]] - V[A[i] - 1][D[i]] - V[C[i]][B[i] - 1] + V[A[i] - 1][B[i] - 1] << endl;
 }
