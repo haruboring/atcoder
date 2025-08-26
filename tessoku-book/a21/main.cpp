5e73e88beb535ed891836b77b413571888ee0ed5
haruboting <raiu_hk@icloud.com>
2025-08-26 22:33:49 +0900

    solve

--- tessoku-book/a21/main.cpp
+++ tessoku-book/a21/main.cpp
@@ -8,4 +8,24 @@
 using namespace std;
 
 signed main() {
+    int N;
+    cin >> N;
+    vector<int> P(N + 1), A(N + 1);
+    rep(i, N) cin >> P[i + 1] >> A[i + 1];
+
+    map<int, int> m;
+    rep(i, N) m[P[i]] += A[i];
+
+    vector<vector<int>> dp(N + 1, vector<int>(N + 1, 0));
+    dp[1][N] = 0;
+    for (int len = N - 2; len >= 0; len--) {
+        for (int r = 1; r + len <= N; r++) {
+            if (r - 1 >= 1) dp[r][r + len] = max(dp[r][r + len], dp[r - 1][r + len] + A[r - 1] * (P[r - 1] >= r && P[r - 1] <= r + len));
+            if (r + len + 1 <= N) dp[r][r + len] = max(dp[r][r + len], dp[r][r + len + 1] + A[r + len + 1] * (P[r + len + 1] >= r && P[r + len + 1] <= r + len));
+        }
+    }
+
+    int ans = 0;
+    rep(i, N) ans = max(ans, dp[i + 1][i + 1]);
+    cout << ans << endl;
 }
--- tessoku-book/b21/main.cpp
+++ tessoku-book/b21/main.cpp
@@ -8,4 +8,22 @@
 using namespace std;
 
 signed main() {
+    int N;
+    cin >> N;
+    string S;
+    cin >> S;
+
+    string rS = S;
+    reverse(all(rS));
+
+    vector<vector<int>> dp(N + 1, vector<int>(N + 1));
+    rep(i, N + 1) {
+        rep(j, N + 1) {
+            if (i < N && j < N && S[i] == rS[j]) dp[i + 1][j + 1] = max(dp[i + 1][j + 1], dp[i][j] + 1);
+            if (i < N) dp[i + 1][j] = max(dp[i + 1][j], dp[i][j]);
+            if (j < N) dp[i][j + 1] = max(dp[i][j + 1], dp[i][j]);
+        }
+    }
+
+    cout << dp[N][N] << endl;
 }
