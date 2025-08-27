401aee4b333623d09570f8918a8da4e9e2c5846a
haruboting <raiu_hk@icloud.com>
2025-08-27 13:21:08 +0900

    solve

--- tessoku-book/a23/main.cpp
+++ tessoku-book/a23/main.cpp
@@ -8,4 +8,26 @@
 using namespace std;
 
 signed main() {
+    int N, M;
+    cin >> N >> M;
+    vector<vector<int>> A(M, vector<int>(N));
+    rep(i, M) rep(j, N) cin >> A[i][j];
+
+    vector<int> Aa(M);
+    rep(i, M) {
+        int t = 0;
+        int k = 1;
+        rep(j, N) {
+            t += k * A[i][j];
+            k *= 2;
+        }
+        Aa[i] = t;
+    }
+
+    vector<int> dist((1 << N), 1e18);
+    dist[0] = 0;
+    rep(i, (1 << N)) rep(j, M) dist[i | Aa[j]] = min(dist[i | Aa[j]], dist[i] + 1);
+
+    if (dist[(1 << N) - 1] >= 1e18) dist[(1 << N) - 1] = -1;
+    cout << dist[(1 << N) - 1] << endl;
 }
--- tessoku-book/b23/main.cpp
+++ tessoku-book/b23/main.cpp
@@ -8,4 +8,33 @@
 using namespace std;
 
 signed main() {
+    int N;
+    cin >> N;
+    vector<double> X(N), Y(N);
+    rep(i, N) cin >> X[i] >> Y[i];
+
+    vector<int> b(N, 1);
+    rep(i, N - 1) b[i + 1] = 2 * b[i];
+
+    // 集合/最後/最初
+    vector<vector<vector<double>>> dp((1 << N), vector<vector<double>>(N, vector<double>(N, 1e18)));
+    rep(i, N) dp[b[i]][i][i] = 0;
+    rep(i, ((1 << N))) {
+        rep(ni, N) {
+            rep(si, N) {
+                rep(li, N) {
+                    dp[i | b[ni]][ni][si] = min(dp[i | b[ni]][ni][si], dp[i][li][si] + sqrt((X[ni] - X[li]) * (X[ni] - X[li]) + (Y[ni] - Y[li]) * (Y[ni] - Y[li])));
+                }
+            }
+        }
+    }
+
+    double ans = 1e18;
+    rep(i, N) {
+        rep(j, N) {
+            dp[(1 << N) - 1][i][j] += sqrt((X[i] - X[j]) * (X[i] - X[j]) + (Y[i] - Y[j]) * (Y[i] - Y[j]));
+            ans = min(ans, dp[(1 << N) - 1][i][j]);
+        }
+    }
+    cout << fixed << setprecision(10) << ans << endl;
 }
