10b483042e583328b56464596f07cfdb694a32f3
haruboting <raiu_hk@icloud.com>
2025-08-29 00:11:46 +0900

    solve

--- tessoku-book/a27/main.cpp
+++ tessoku-book/a27/main.cpp
@@ -8,4 +8,8 @@
 using namespace std;
 
 signed main() {
+    int A, B;
+    cin >> A >> B;
+
+    cout << gcd(A, B) << endl;
 }
--- tessoku-book/b27/main.cpp
+++ tessoku-book/b27/main.cpp
@@ -8,4 +8,8 @@
 using namespace std;
 
 signed main() {
+    int A, B;
+    cin >> A >> B;
+
+    cout << A * B / gcd(A, B) << endl;
 }
